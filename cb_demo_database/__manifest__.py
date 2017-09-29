d89be413f2248cd6826e443fc1efcc375909a08d
rgarnau <rgarnau20@gmail.com>
2017-09-29 13:57:22 +0200

    [IMP] some improvements

--- cb_demo_database/__manifest__.py
+++ cb_demo_database/__manifest__.py
@@ -1,27 +1,32 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2017 LasLabs Inc.
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'Creu Blanca Demo Database',
-    'summary': '',
     'version': '10.0.1.0.0',
-    'author': 'Eficent, Odoo Community Association (OCA)',
-    'category': '',
+    'author': 'Eficent',
     'depends': [
+        'workflow_plandefinition',
+        'medical_request_group',
+        'medical_careplan',
+        'medical_encounter',
+        'medical_encounter_careplan',
+        'medical_procedure',
+        'medical_insurance_sale',
+        'medical_careplan_plandefinition',
+        'medical_material',
+        'medical_careplan_sale',
+        'medical_practitioner',
+        'medical_procedure_purchase',
     ],
     'demo': [
-        'demo/prova_workflow_types.xml',
-        'demo/product_category_demo.xml',
-        'demo/products_demo.xml',
-        'demo/prova_act_def.xml',
-        'demo/prova_plan_definition.xml',
-        'demo/center_demo.xml',
-        'demo/prova_insurance.xml',
-        'demo/sale_commission_formula_demo.xml',
-        'demo/practitioner_demo.xml',
+        'demo_data/workflow_type.xml',
+        'demo_data/product_category.xml',
+        'demo_data/products.xml',
+        'demo_data/activity_definition.xml',
+        'demo_data/plan_definition.xml',
     ],
-    'website': '',
     'licence': 'LGPL-3',
     'installable': True,
     'auto_install': False,
