b23d93bfbbbba0130eff6c539a6ac35e247f5e9c
rgarnau <rgarnau20@gmail.com>
2017-11-27 10:21:28 +0100

    [ADD] Prefix 'cb_' in all modules plus add formula data to medical_commission

---
+++ cb_medical_workflow_plandefinition/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Medical Plan Definition',
+    'version': '11.0.1.0.0',
+    'author': 'Eficent, Creu Blanca, Odoo Community Association (OCA)',
+    'depends': [
+        'medical_workflow',
+    ],
+    'data': [
+        'views/workflow_plan_definition_action.xml',
+        'views/workflow_plan_definition.xml',
+    ],
+    'website': 'https://github.com/OCA/vertical-medical',
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
