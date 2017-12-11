f8a575b96d7f072b93accc7abb2b6b81af020501
Enric Tobella <etobella@creublanca.es>
2017-12-11 17:51:12 +0100

    [ADD] cb_medical_administration_practitioner_specialty

---
+++ cb_medical_administration_practitioner_specialty/models/medical_role.py
@@ -0,0 +1,16 @@
+# Copyright 2017 LasLabs Inc.
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class MedicalRole(models.Model):
+    # FHIR Entity: PractitionerRole
+    # (https://www.hl7.org/fhir/practitionerrole.html)
+    _inherit = 'medical.role'
+
+    specialty_required = fields.Boolean(
+        default=False
+    )
---
+++ cb_medical_administration_practitioner_specialty/tests/test_specialty.py
@@ -0,0 +1,28 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestMedicalCommission(TransactionCase):
+
+    def setUp(self):
+        super(TestMedicalCommission, self).setUp()
+        self.specialty = self.env['medical.specialty'].create({
+            'name': 'Trauma',
+            'description': 'Traumatology',
+            'code': 'TRA'
+        })
+        self.doctor = self.browse_ref(
+            'medical_administration_practitioner.doctor')
+
+    def test_create_practitioner(self):
+        self.specialty.sequence_number_next = 21
+        practitoner = self.env['res.partner'].create({
+            'name': 'Doctor',
+            'is_practitioner': True,
+            'practitioner_role_id': self.doctor.id,
+            'specialty_id': self.specialty.id
+        })
+        self.assertEqual(practitoner.practitioner_identifier, 'TRA021')
