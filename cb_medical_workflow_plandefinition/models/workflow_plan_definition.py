63456665fb5ff9864a8080d7df4e4a71d7c48fdd
Enric Tobella <etobella@creublanca.es>
2017-12-27 16:50:30 +0100

    Improve workflow

--- cb_medical_workflow_plandefinition/models/workflow_plan_definition.py
+++ cb_medical_workflow_plandefinition/models/workflow_plan_definition.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PlanDefinition(models.Model):
@@ -16,3 +16,28 @@ class PlanDefinition(models.Model):
         string='Possible breakdown?',
         default=True,
     )
+
+    def get_request_group_vals(self, vals):
+        agreement_item_id = self.env['medical.coverage.agreement.item'].browse(
+            vals.get('coverage_agreement_item_id')
+        )
+        values = vals.copy()
+        values['service_id'] = agreement_item_id.product_id.id
+        values['is_billable'] = self.is_billable
+        values['is_breakdown'] = self.is_breakdown
+        return values
+
+    @api.multi
+    def execute_plan_definition(self, vals, parent=False):
+        self.ensure_one()
+        request_group = parent
+        if (
+            not request_group and
+            not self.activity_definition_id and
+            vals.get('coverage_agreement_item_id', False)
+        ):
+            request_group = self.env['medical.request.group'].create(
+                self.get_request_group_vals(vals)
+            )
+        return super(PlanDefinition, self).execute_plan_definition(
+            vals, request_group)
