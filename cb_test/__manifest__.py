04bee0f0b74efebe31007aa25a65b3d6df6d39b8
Enric Tobella <etobella@creublanca.es>
2018-01-10 17:26:50 +0100

    [ADD] cb_test

---
+++ cb_test/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'CB Testing',
+    'version': '11.0.1.0.0',
+    'author': 'Eficent, Creu Blanca, Odoo Community Association (OCA)',
+    'depends': [
+        'cb_medical_careplan_sale',
+        'cb_medical_commission',
+        'cb_medical_pos',
+        'cb_medical_sale_invoice_group_method',
+    ],
+    'data': [
+    ],
+    'website': 'https://github.com/OCA/vertical-medical',
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
