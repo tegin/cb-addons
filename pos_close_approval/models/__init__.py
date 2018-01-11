02c53388f69e733fce53b5b87c6fd9866f14ecd5
Enric Tobella <etobella@creublanca.es>
2018-01-11 10:22:29 +0100

    [ADD] pos_close_approval

---
+++ pos_close_approval/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import pos_config
+from . import pos_session
+from . import account_bank_statement_line
