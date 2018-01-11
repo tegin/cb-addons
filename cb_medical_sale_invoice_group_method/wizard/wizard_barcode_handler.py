d9c52ada8381370a19af73ad6fa4e8cdad1b14b9
rgarnau <rgarnau20@gmail.com>
2018-01-11 13:33:38 +0100

    [imp] barcode

--- cb_medical_sale_invoice_group_method/wizard/wizard_barcode_handler.py
+++ cb_medical_sale_invoice_group_method/wizard/wizard_barcode_handler.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models, _
 
 
 class WizardSalePreinvoiceGroup(models.TransientModel):
@@ -18,26 +18,57 @@ class WizardSalePreinvoiceGroup(models.TransientModel):
         comodel_name='sale.order.line',
         related='preinvoice_group_id.line_ids'
     )
-    status = fields.Char(readonly=1)
-    status_state = fields.Integer(default=0, readonly=1, )
+    status = fields.Text(
+        readonly=1,
+        default="Start scanning",
+    )
+    status_state = fields.Integer(
+        default=0,
+        readonly=1,
+    )
+    encounter_id = fields.Many2one(
+        'medical.encounter',
+        computed='on_barcode_scanned',
+    )
+    processed_lines = fields.Many2many(
+        'sale.order.line',
+        computed='on_barcode_scanned',
+    )
+
+    def _show_lines(self):
+        show_lines = _("The following lines have been processed from "
+                       "Encounter %s:\n") % self.encounter_id.display_name
+        for line in self.processed_lines:
+            show_lines = "%s %s [%s]\n" %(
+                show_lines, line.product_id.name, line.order_id.name)
+        return show_lines + _("Scan the next barcode or press Close to "
+                              "finish scanning.")
 
     def on_barcode_scanned(self, barcode):
-        encounter_id = self.env['medical.encounter'].search([
+        self.encounter_id = self.env['medical.encounter'].search([
             ('internal_identifier', '=', barcode)
         ])
-        if not encounter_id:
-            self.status = 'Encounter not found'
+        if not self.encounter_id:
+            self.status = _("Barcode %s does not correspond to any "
+                            "Encounter. Try with another barcode or "
+                            "press Close to finish scanning.") % \
+                          barcode
             self.status_state = 1
             return
-        lines = self.line_ids.filtered(
-            lambda r: r.encounter_id.id == encounter_id.id
+        self.processed_lines = self.line_ids.filtered(
+            lambda r: r.encounter_id.id == self.encounter_id.id and not
+            r.is_validated
         )
-        if not lines:
-            self.status = 'Lines not found'
+        if not self.processed_lines:
+            self.status = _("The Encounter %s does not belong to this "
+                            "pre-invoice group. Try with another barcode or "
+                            "press Close to finish scanning.")\
+                          % \
+                          self.encounter_id.display_name
             self.status_state = 1
             return
-        for line in lines:
+        for line in self.processed_lines:
             self.preinvoice_group_id.validate_line(line)
-            self.status = 'OK'
+            self.status = self._show_lines()
             self.status_state = 0
         self.preinvoice_group_id._compute_lines()
