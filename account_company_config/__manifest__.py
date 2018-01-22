066118e4b89ece2bd550742b53f134d0f10e2847
Enric Tobella <etobella@creublanca.es>
2018-01-22 10:13:33 +0100

    [ADD] account_company_config

---
+++ account_company_config/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Intercompany journal',
+    'version': '11.0.1.0.0',
+    'author': 'Eficent, Creu Blanca, Odoo Community Association (OCA)',
+    'website': 'http://github.com/eficent/cb-addons',
+    'summary': 'Creates inter company relations',
+    'sequence': 30,
+    'category': 'Accounting',
+    'depends': ['account', 'base'],
+    'license': 'LGPL-3',
+    'data': [
+        'views/res_company_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
