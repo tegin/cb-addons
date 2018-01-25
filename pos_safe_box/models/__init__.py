caba95e2d29d82dcd8d26582ea3751cd1857b2c7
Enric Tobella <etobella@creublanca.es>
2018-01-25 12:47:38 +0100

    [ADD] pos_safe_box

---
+++ pos_safe_box/models/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import pos_session
+from . import pos_config
+from . import pos_session_validation
+from . import safe_box_group
+from . import safe_box_coin
---
+++ pos_safe_box/models/pos_session.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, fields, models
+
+
+class PosSession(models.Model):
+    _inherit = 'pos.session'
+
+    pos_session_validation_id = fields.Many2one(
+        'pos.session.validation',
+        readonly=True,
+    )
+
+    @api.multi
+    def action_pos_session_close(self):
+        res = super(PosSession, self).action_pos_session_close()
+        for session in self:
+            sbg = session.config_id.safe_box_group_id
+            if sbg:
+                self.pos_session_validation_id = sbg.\
+                    get_current_session_validation()
+        return res
