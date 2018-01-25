e477121864a230690f78b81d743f00d151a567f8
Enric Tobella <etobella@creublanca.es>
2018-01-25 12:47:38 +0100

    [ADD] pos_safe_box

---
+++ pos_safe_box/models/pos_config.py
@@ -0,0 +1,14 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
+
+    safe_box_group_id = fields.Many2one(
+        'safe.box.group',
+        string='Safe box system'
+    )
