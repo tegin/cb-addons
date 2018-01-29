dedeb3f063f17c8d14ad51cbb944065aae13fe0f
Enric Tobella <etobella@creublanca.es>
2018-01-29 13:25:27 +0100

    [FIX] remove unused files

--- cb_medical_commission/models/__init__.py
+++ cb_medical_commission/models/__init__.py
@@ -2,13 +2,13 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from . import sale_order
-from . import sale_order_line
 from . import account_invoice
-from . import product_template
+from . import medical_careplan
 from . import medical_procedure
 from . import medical_procedure_request
-from . import workflow_plan_definition_action
+from . import product_template
 from . import res_partner
+from . import sale_order
+from . import sale_order_line
 from . import workflow_activity_definition
-from . import medical_careplan
+from . import workflow_plan_definition_action
