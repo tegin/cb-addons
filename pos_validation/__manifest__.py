54bb8ed25dfd5df47de1ae985c6ba16c985a311a
Enric Tobella <etobella@creublanca.es>
2018-01-29 14:21:46 +0100

    [ADD] pos_validation

---
+++ pos_validation/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'PoS Validation',
+    'version': '11.0.1.0.0',
+    'category': 'Reporting',
+    'website': 'https://github.com/eficent/cb-addons',
+    'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'summary': 'Validation of Careplans once they are assigned to a Session',
+    'depends': [
+        'cb_medical_pos',
+        'pos_safe_box',
+        'cb_medical_commission',
+    ],
+    'data': [
+        'views/administration_menu.xml',
+        'views/pos_session_views.xml',
+    ]
+}
