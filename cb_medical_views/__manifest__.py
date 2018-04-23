a4c20e396d24cda6b55d78fc0a2f304d9f50e107
Enric Tobella <etobella@creublanca.es>
2018-04-23 12:17:46 +0200

    [ADD] cb_medical_views

---
+++ cb_medical_views/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'CB Medical Views',
+    'version': '11.0.1.0.0',
+    'author': 'Eficent, Creu Blanca, Odoo Community Association (OCA)',
+    'depends': [
+        'medical_workflow',
+        'medical_administration_encounter',
+    ],
+    'data': [
+        'views/medical_encounter.xml',
+        'views/medical_event_view.xml',
+        'views/medical_request_views.xml',
+    ],
+    'website': 'https://github.com/OCA/vertical-medical',
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
