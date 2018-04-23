0733cf9bfb8863fade43017832cad938e3c4a319
Enric Tobella <etobella@creublanca.es>
2018-04-23 12:16:37 +0200

    [ADD] coverage_magnetic_str

---
+++ cb_medical_coverage_magnetic_str/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2017 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'CB Medical Coverage Magnetic str',
+    'version': '11.0.1.0.0',
+    'category': 'CB',
+    'website': 'https://creublanca.es',
+    'author': 'Creu Blanca',
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'summary': 'CB medical location data',
+    'depends': [
+        'medical_financial_coverage',
+    ],
+}
