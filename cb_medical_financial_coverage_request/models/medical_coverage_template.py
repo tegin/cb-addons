b66374d07d879f9b91ec8fd4bdad2cdafbffaa8b
Enric Tobella <etobella@creublanca.es>
2018-04-23 13:34:41 +0200

    [IMP] Add track_visibility on required fields.

--- cb_medical_financial_coverage_request/models/medical_coverage_template.py
+++ cb_medical_financial_coverage_request/models/medical_coverage_template.py
@@ -4,5 +4,10 @@ from odoo import models, fields
 class MedicalCoverageTemplate(models.Model):
     _inherit = 'medical.coverage.template'
 
-    subscriber_required = fields.Boolean(default=False)
-    subscriber_format = fields.Char()
+    subscriber_required = fields.Boolean(
+        track_visibility=True,
+        default=False,
+    )
+    subscriber_format = fields.Char(
+        track_visibility=True,
+    )
