8555730247d4c0b30fe7725dab806f28394575ca
Enric Tobella <etobella@creublanca.es>
2018-04-25 13:18:19 +0200

    [ADD] medical_document

---
+++ medical_document/models/medical_document_reference.py
@@ -0,0 +1,84 @@
+# Copyright 2018 Creu Blanca
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class MedicalDocumentReference(models.Model):
+    # FHIR Entity: Document Reference
+    # (https://www.hl7.org/fhir/documentreference.html)
+    _name = 'medical.document.reference'
+    _description = 'Medical Document Reference'
+    _inherit = 'medical.request'
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('current', 'Current'),
+        ('superseded', 'Superseded')
+    ], required=True, track_visibility=True, default='draft')
+    document_type_id = fields.Many2one(
+        'medical.document.type',
+        required=True,
+        readonly=True,
+        ondelete='restrict',
+    )
+    document_template_id = fields.Many2one(
+        'medical.document.template',
+        readonly=True,
+        ondelete='restrict',
+    )
+    is_editable = fields.Boolean(
+        compute='_compute_is_editable',
+    )
+    text = fields.Html(
+        string='Document text',
+        readonly=True,
+        sanitize=True
+    )
+
+    def _get_internal_identifier(self, vals):
+        return self.env['ir.sequence'].next_by_code(
+            'medical.document.reference') or '/'
+
+    @api.multi
+    @api.depends('state')
+    def _compute_is_editable(self):
+        for rec in self:
+            rec.is_editable = bool(rec.state == 'draft')
+
+    def action_view_request_parameters(self):
+        return {
+            'view': 'medical_document.medical_document_reference_action',
+            'view_form': 'medical.document.reference.view.form', }
+
+    def _get_parent_field_name(self):
+        return 'document_reference_id'
+
+    @api.multi
+    def print(self):
+        self.ensure_one()
+        if self.state == 'draft':
+            return self.draft2current()
+        return self.print_template()
+
+    def print_template(self):
+        return self.document_type_id.report_action_id.report_action(self)
+
+    @api.multi
+    def draft2current(self):
+        self.ensure_one()
+        if self.state != 'draft':
+            raise ValidationError(_('State must be draft'))
+        self.document_template_id = self.document_type_id.current_template_id
+        self.text = self.document_template_id.render_template(
+            self._name, self.id
+            )
+        self.write({'state': 'current'})
+        return self.print_template()
+
+    @api.multi
+    def current2superseded(self):
+        if self.filtered(lambda r: r.state != 'current'):
+            raise ValidationError(_('State must be Current'))
+        self.write({'state': 'superseded'})
---
+++ medical_document/models/workflow_activity_definition.py
@@ -0,0 +1,38 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, fields, models
+
+
+class ActivityDefinition(models.Model):
+    _inherit = 'workflow.activity.definition'
+
+    requires_document_template = fields.Boolean(
+        compute='_compute_requires_document_template'
+    )
+    document_type_id = fields.Many2one(
+        'medical.document.type',
+        domain=[('state', '=', 'current')],
+        ondelete='restrict',
+    )
+
+    @api.depends('model_id')
+    def _compute_requires_document_template(self):
+        for record in self:
+            record.requires_document_template = bool(
+                record.model_id.model == 'medical.document.reference'
+            )
+
+    @api.onchange('model_id')
+    def _onchange_model(self):
+        if self.model_id.model != 'medical.document.reference':
+            self.document_type_id = False
+
+    def _get_medical_values(self, vals, parent=False, plan=False, action=False
+                            ):
+        values = super(ActivityDefinition, self)._get_medical_values(
+            vals, parent, plan, action)
+        if self.model_id.model == 'medical.document.reference':
+            values['document_type_id'] = self.document_type_id.id
+        return values
