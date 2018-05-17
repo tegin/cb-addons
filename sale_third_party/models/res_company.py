3b8e5a7655170d8a5c97b8d63dd76925a19008e9
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-05-17 18:16:08 +0200

    refactor third party move status and residual amounts

--- sale_third_party/models/res_company.py
+++ sale_third_party/models/res_company.py
@@ -6,14 +6,16 @@ class ResCompany(models.Model):
 
     default_third_party_customer_account_id = fields.Many2one(
         'account.account',
-        domain="[('deprecated', '=', False),('company_id', '=', active_id)]",
+        domain="[('deprecated', '=', False),('company_id', '=', active_id),"
+               "('internal_type', '=', 'receivable')]",
         compute='_compute_third_party_customer_account',
         inverse='_inverse_third_party_customer_account',
         string="Default Account for customers in third party sales"
     )
     default_third_party_supplier_account_id = fields.Many2one(
         'account.account',
-        domain="[('deprecated', '=', False),('company_id', '=', active_id)]",
+        domain="[('deprecated', '=', False),('company_id', '=', active_id),"
+               "('internal_type', '=', 'payable')]",
         compute='_compute_third_party_supplier_account',
         inverse='_inverse_third_party_supplier_account',
         string="Default Account for suppliers in third party sales",
