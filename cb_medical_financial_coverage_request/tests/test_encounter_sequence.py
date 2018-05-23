ca4012990334f290e02583c8f80f101faac344ef
Enric Tobella <etobella@creublanca.es>
2018-05-23 15:15:36 +0200

    [IMP] add sequence prefix for encounter

---
+++ cb_medical_financial_coverage_request/tests/test_encounter_sequence.py
@@ -0,0 +1,64 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestCoverage(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.patient = self.env['medical.patient'].create({
+            'name': 'Patient',
+        })
+        self.sequence = self.browse_ref(
+            'medical_administration_encounter.seq_medical_encounter')
+
+    def test_write_sequence(self):
+        center = self.env['res.partner'].create({
+            'name': 'Center',
+            'is_center': True,
+        })
+        self.assertFalse(center.encounter_sequence_id)
+        code = self.sequence.get_next_char(self.sequence.number_next_actual)
+        encounter = self.env['medical.encounter'].create({
+            'patient_id': self.patient.id,
+            'center_id': center.id,
+        })
+        self.assertEqual(encounter.internal_identifier, code)
+        center.write({'encounter_sequence_prefix': 'R'})
+        self.assertEqual(center.encounter_sequence_id.prefix, 'R')
+        self.assertTrue(center.encounter_sequence_id)
+        code = center.encounter_sequence_id.get_next_char(
+            center.encounter_sequence_id.number_next_actual)
+        encounter = self.env['medical.encounter'].create({
+            'patient_id': self.patient.id,
+            'center_id': center.id,
+        })
+        self.assertEqual(encounter.internal_identifier, code)
+
+    def test_create_sequence(self):
+        center = self.env['res.partner'].create({
+            'name': 'Center',
+            'is_center': True,
+            'encounter_sequence_prefix': 'S',
+        })
+        self.assertTrue(center.encounter_sequence_id)
+        self.assertEqual(center.encounter_sequence_id.prefix, 'S')
+        code = center.encounter_sequence_id.get_next_char(
+            center.encounter_sequence_id.number_next_actual)
+        encounter = self.env['medical.encounter'].create({
+            'patient_id': self.patient.id,
+            'center_id': center.id,
+        })
+        self.assertEqual(encounter.internal_identifier, code)
+        center.write({'encounter_sequence_prefix': 'R'})
+        self.assertEqual(center.encounter_sequence_id.prefix, 'R')
+        self.assertTrue(center.encounter_sequence_id)
+        code = center.encounter_sequence_id.get_next_char(
+            center.encounter_sequence_id.number_next_actual)
+        encounter = self.env['medical.encounter'].create({
+            'patient_id': self.patient.id,
+            'center_id': center.id,
+        })
+        self.assertEqual(encounter.internal_identifier, code)
