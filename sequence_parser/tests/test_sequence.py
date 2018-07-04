04b9f818b5155a40029fc7871cb124c7f55478ef
Enric Tobella <etobella@creublanca.es>
2018-07-04 15:25:10 +0200

    [ADD] sequence_parser

---
+++ sequence_parser/tests/test_sequence.py
@@ -0,0 +1,55 @@
+from odoo.tests.common import TransactionCase
+
+
+class TestSequence(TransactionCase):
+
+    def create_sequence(self, prefix, suffix):
+        return self.env['ir.sequence'].create({
+            'name': 'sequence',
+            'prefix': prefix,
+            'suffix': suffix,
+            'padding': 2,
+        })
+
+    def test_standard(self):
+        sequence = self.create_sequence('P', 'S')
+        self.assertTrue(isinstance(sequence.next_by_id(), str))
+        result = sequence.with_context(sequence_tuple=True).next_by_id()
+        self.assertTrue(isinstance(result, tuple))
+        prefix, value, suffix, seq = result
+        self.assertEqual(prefix, 'P')
+        self.assertEqual(suffix, 'S')
+        self.assertTrue(isinstance(value, int))
+
+    def test_no_gap(self):
+        sequence = self.create_sequence('P', 'S')
+        sequence.write({'implementation': 'no_gap'})
+        self.assertTrue(isinstance(sequence.next_by_id(), str))
+        result = sequence.with_context(sequence_tuple=True).next_by_id()
+        self.assertTrue(isinstance(result, tuple))
+        prefix, value, suffix, seq = result
+        self.assertEqual(prefix, 'P')
+        self.assertEqual(suffix, 'S')
+        self.assertTrue(isinstance(value, int))
+
+    def test_standard_date(self):
+        sequence = self.create_sequence('P', 'S')
+        sequence.write({'use_date_range': True})
+        self.assertTrue(isinstance(sequence.next_by_id(), str))
+        result = sequence.with_context(sequence_tuple=True).next_by_id()
+        self.assertTrue(isinstance(result, tuple))
+        prefix, value, suffix, seq = result
+        self.assertEqual(prefix, 'P')
+        self.assertEqual(suffix, 'S')
+        self.assertTrue(isinstance(value, int))
+
+    def test_no_gap_date(self):
+        sequence = self.create_sequence('P', 'S')
+        sequence.write({'implementation': 'no_gap', 'use_date_range': True})
+        self.assertTrue(isinstance(sequence.next_by_id(), str))
+        result = sequence.with_context(sequence_tuple=True).next_by_id()
+        self.assertTrue(isinstance(result, tuple))
+        prefix, value, suffix, seq = result
+        self.assertEqual(prefix, 'P')
+        self.assertEqual(suffix, 'S')
+        self.assertTrue(isinstance(value, int))
