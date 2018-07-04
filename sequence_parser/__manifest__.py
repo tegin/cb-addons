6d11eac65696b3c776ba7cab9c9afb928d2c2223
Enric Tobella <etobella@creublanca.es>
2018-07-04 15:25:10 +0200

    [ADD] sequence_parser

---
+++ sequence_parser/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Sequence parser',
+    'version': '11.0.1.0.0',
+    'author': 'Eficent, Creu Blanca, Odoo Community Association (OCA)',
+    'website': 'http://github.com/eficent/cb-addons',
+    'summary': 'Returns the sequence on a tuple',
+    'sequence': 30,
+    'category': 'Sale',
+    'depends': [
+        'base',
+    ],
+    'license': 'LGPL-3',
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
