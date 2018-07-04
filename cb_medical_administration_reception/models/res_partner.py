a572d0c20d19f873ecffe3cb2aea01f7a962b766
Enric Tobella <etobella@creublanca.es>
2018-07-04 15:17:43 +0200

    [ADD] medical reception

---
+++ cb_medical_administration_reception/models/res_partner.py
@@ -0,0 +1,42 @@
+# Copyright 2017 LasLabs Inc.
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class ResPartner(models.Model):
+    # FHIR Entity: Location (https://www.hl7.org/fhir/location.html)
+    _inherit = 'res.partner'
+
+    is_reception = fields.Boolean(
+        default=False,
+    )
+    reception_identifier = fields.Char(
+        readonly=True,
+    )  # FHIR Field: identifier
+
+    @api.constrains('is_reception', 'center_id')
+    def check_reception_center(self):
+        if self.is_reception and not self.center_id:
+            raise ValidationError(_(
+                'Center must be fullfilled on receptions'
+            ))
+
+    @api.model
+    def _get_medical_identifiers(self):
+        res = super(ResPartner, self)._get_medical_identifiers()
+        res.append((
+            'is_medical',
+            'is_reception',
+            'reception_identifier',
+            self._get_reception_identifier
+        ))
+        return res
+
+    @api.model
+    def _get_reception_identifier(self, vals):
+        return self.env['ir.sequence'].next_by_code(
+            'medical.reception') or '/'
