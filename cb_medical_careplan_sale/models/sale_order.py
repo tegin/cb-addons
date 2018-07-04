89de9ac7ef38e5138cff953e7ed23bd6f02e68a9
Enric Tobella <etobella@creublanca.es>
2018-07-04 15:18:08 +0200

    [IMP] careplan_sale: Add coverage on computation of SO

--- cb_medical_careplan_sale/models/sale_order.py
+++ cb_medical_careplan_sale/models/sale_order.py
@@ -8,15 +8,14 @@ from odoo import fields, models
 class SaleOrder(models.AbstractModel):
     _inherit = 'sale.order'
 
-    encounter_id = fields.Many2one(
-        'medical.encounter',
+    encounter_id = fields.Many2one('medical.encounter', readonly=True, )
+    coverage_id = fields.Many2one('medical.coverage', readonly=True, )
+    coverage_template_id = fields.Many2one(
+        'medical.coverage.template', readonly=True,
+        related='coverage_id.coverage_template_id'
     )
-    coverage_agreement_id = fields.Many2one(
-        'medical.coverage.agreement',
-    )
-    patient_id = fields.Many2one(
-        'medical.patient'
-    )
+    coverage_agreement_id = fields.Many2one('medical.coverage.agreement')
+    patient_id = fields.Many2one('medical.patient', readonly=True, )
 
     def create_third_party_move(self):
         if self.coverage_agreement_id:
--- cb_medical_careplan_sale/tests/__init__.py
+++ cb_medical_careplan_sale/tests/__init__.py
@@ -1,4 +1,5 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
+from . import test_coverage
 from . import test_sub_payor
 from . import test_config_settings
