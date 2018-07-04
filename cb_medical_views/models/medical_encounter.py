c11365199bfcddd5b4ed517e75f9827319499b16
Enric Tobella <etobella@creublanca.es>
2018-07-04 15:25:29 +0200

    [IMP] cb_medical_view: Find encounters

---
+++ cb_medical_views/models/medical_encounter.py
@@ -0,0 +1,34 @@
+from odoo import api, models, _
+from odoo.tools.safe_eval import safe_eval
+import json
+
+
+class MedicalEncounter(models.Model):
+    _inherit = 'medical.encounter'
+
+    @api.multi
+    def find_encounter_by_barcode(self, barcode):
+        encounter = self.search([('internal_identifier', '=', barcode)])
+        if not encounter:
+            document = self.env['medical.document.reference'].search([
+                ('internal_identifier', '=', barcode)])
+            if document:
+                encounter = document.encounter_id
+        if not encounter:
+            action = self.env.ref(
+                'cb_medical_views.encounter_find_by_barcode')
+            result = action.read()[0]
+            context = safe_eval(result['context'])
+            context.update({
+                'default_state': 'warning',
+                'default_status': _('Encounter %s cannot be found') % barcode
+            })
+            result['context'] = json.dumps(context)
+            return result
+        action = self.env.ref(
+            'medical_administration_encounter.medical_encounter_action')
+        result = action.read()[0]
+        res = self.env.ref('medical_encounter.medical_encounter_form', False)
+        result['views'] = [(res and res.id or False, 'form')]
+        result['res_id'] = encounter.id
+        return result
