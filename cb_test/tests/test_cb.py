56dd9c82f8ae9b21b4784a6f0e339f2c050e5096
Enric Tobella <etobella@creublanca.es>
2018-07-09 16:21:17 +0200

    [FIX] Integration between down payments and third parties

--- cb_test/tests/test_cb.py
+++ cb_test/tests/test_cb.py
@@ -963,3 +963,78 @@ class TestMedicalCareplanSale(TransactionCase):
         self.assertTrue(action.get('res_id', False))
         for sale_order in sale_orders:
             self.assertTrue(sale_order.invoice_status == 'invoiced')
+
+    def test_down_payments(self):
+        self.plan_definition2.third_party_bill = False
+        encounter, careplan, group = self.create_careplan_and_group(
+            self.agreement_line3
+        )
+        self.env['wizard.medical.encounter.add.amount'].create({
+            'encounter_id': encounter.id,
+            'pos_session_id': self.session.id,
+            'journal_id': self.journal_1[0].id,
+            'amount': 100
+        }).run()
+        self.env['wizard.medical.encounter.close'].create({
+            'encounter_id': encounter.id,
+            'pos_session_id': self.session.id,
+        }).run()
+        self.assertEqual(encounter.pending_private_amount, 0)
+        self.env['wizard.medical.encounter.finish'].create({
+            'encounter_id': encounter.id,
+            'pos_session_id': self.session.id,
+            'journal_id': self.journal_1[0].id,
+        }).run()
+        sale_order = encounter.sale_order_ids.filtered(
+            lambda r: not r.is_down_payment)
+        self.assertTrue(sale_order)
+        self.assertEqual(sale_order.amount_total, 0)
+
+    def test_down_payments_third_party(self):
+        self.plan_definition2.third_party_bill = True
+        encounter, careplan, group = self.create_careplan_and_group(
+            self.agreement_line3
+        )
+        self.env['wizard.medical.encounter.add.amount'].create({
+            'encounter_id': encounter.id,
+            'pos_session_id': self.session.id,
+            'journal_id': self.journal_1[0].id,
+            'amount': 100
+        }).run()
+        for request in group.procedure_request_ids:
+            request.draft2active()
+            self.assertEqual(request.center_id, encounter.center_id)
+            procedure = request.generate_event()
+            procedure.performer_id = self.practitioner_01
+            procedure.commission_agent_id = self.practitioner_01
+            procedure.performer_id = self.practitioner_02
+            procedure._onchange_performer_id()
+            self.assertEqual(
+                procedure.commission_agent_id, self.practitioner_02)
+        self.practitioner_02.third_party_sequence_id = self.env[
+            'ir.sequence'].create({'name': 'sequence'})
+        self.env['wizard.medical.encounter.close'].create({
+            'encounter_id': encounter.id,
+            'pos_session_id': self.session.id,
+        }).run()
+        self.assertEqual(encounter.pending_private_amount, 0)
+        self.env['wizard.medical.encounter.finish'].create({
+            'encounter_id': encounter.id,
+            'pos_session_id': self.session.id,
+            'journal_id': self.journal_1[0].id,
+        }).run()
+        self.assertEqual(encounter.pending_private_amount, 0)
+        sale_order = encounter.sale_order_ids.filtered(
+            lambda r: not r.is_down_payment and not r.third_party_partner_id)
+        self.assertTrue(sale_order)
+        self.assertEqual(sale_order.amount_total, -100)
+        payments = sale_order.invoice_ids.mapped('bank_statement_line_ids')
+        self.assertTrue(payments)
+        self.assertEqual(-100, payments.amount)
+        sale_order = encounter.sale_order_ids.filtered(
+            lambda r: not r.is_down_payment and r.third_party_partner_id)
+        self.assertTrue(sale_order)
+        payments = sale_order.mapped('bank_statement_line_ids')
+        self.assertTrue(payments)
+        self.assertEqual(100, payments.amount)
+        self.assertEqual(sale_order.amount_total, 100)
