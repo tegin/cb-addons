fdcaa06031d8f5e20c2ca31e536a2a5a5448571c
Enric Tobella <etobella@creublanca.es>
2018-07-09 11:50:00 +0200

    [IMP] validation views

--- cb_medical_sale_invoice/models/sale_order.py
+++ cb_medical_sale_invoice/models/sale_order.py
@@ -5,7 +5,6 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     patient_name = fields.Char(
-        readonly=True,
         states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}
     )
 
@@ -21,18 +20,34 @@ class SaleOrder(models.Model):
                 res['show_authorization'] = p.show_authorization
         return res
 
+    @api.multi
+    @api.onchange('patient_name')
+    def _onchange_patient_name(self):
+        for rec in self:
+            for line in rec.order_line.filtered(lambda r: r != rec):
+                line.patient_name = rec.patient_name
+
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    authorization_number = fields.Char(readonly=True)
-    subscriber_id = fields.Char(readonly=True)
+    authorization_number = fields.Char()
+    subscriber_id = fields.Char()
+    patient_name = fields.Char()
+
+    @api.multi
+    @api.onchange('patient_name')
+    def _onchange_patient_name(self):
+        for rec in self:
+            rec.order_id.patient_name = rec.patient_name
+            for line in rec.order_id.order_line.filtered(lambda r: r != rec):
+                line.patient_name = rec.patient_name
 
     @api.multi
     def _prepare_invoice_line(self, qty):
         res = super()._prepare_invoice_line(qty)
         if self.encounter_id:
-            res['patient_name'] = self.order_id.patient_name
+            res['patient_name'] = self.patient_name
             res['subscriber_id'] = self.subscriber_id
             res['encounter_id'] = self.encounter_id.id
             res['authorization_number'] = self.authorization_number
