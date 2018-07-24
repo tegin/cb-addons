9e7013b557f0c25acdd2ac0d182fb3bc73407378
Enric Tobella <etobella@creublanca.es>
2018-07-24 16:33:19 +0200

    fixup! [FIX] Remove failure when authorization number is not correct. The data will be checked later.

--- cb_medical_financial_coverage_request/tests/test_wizard.py
+++ cb_medical_financial_coverage_request/tests/test_wizard.py
@@ -3,7 +3,6 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo.tests.common import TransactionCase
-from odoo.exceptions import ValidationError
 
 
 class TestWizard(TransactionCase):
