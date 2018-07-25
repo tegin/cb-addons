a3ce38be25e9117419a461554639412323d377dc
Enric Tobella <etobella@creublanca.es>
2018-07-25 18:19:49 +0200

    [ADD] Product_nomenclature

---
+++ product_nomenclature/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Product nomenclature',
+    'version': '11.0.1.0.0',
+    'author': 'Creu Blanca, Odoo Community Association (OCA)',
+    'category': 'Product',
+    'depends': [
+        'product',
+        'sales_team',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_nomenclature_product_views.xml',
+        'views/product_nomenclature_views.xml',
+        'views/product_views.xml',
+    ],
+    'website': 'https://github.com/Eficent/cb-addons',
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
