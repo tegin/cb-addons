d24ebccaccb432a1af532e79cd25ba0953d7dfb2
Enric Tobella <etobella@creublanca.es>
2018-07-26 10:28:05 +0200

    [FIX] Change product.template for product.product on nomenclatures

--- product_nomenclature/models/__init__.py
+++ product_nomenclature/models/__init__.py
@@ -2,5 +2,5 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from . import product_template
+from . import product
 from . import product_nomenclature
