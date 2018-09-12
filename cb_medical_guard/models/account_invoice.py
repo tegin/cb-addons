f0a0be5a202c6310806ff6f643d91edfb952cd4c
Enric Tobella <etobella@creublanca.es>
2018-09-12 15:26:32 +0200

    [ADD] cb_medical_guard

---
+++ cb_medical_guard/models/account_invoice.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    guard_id = fields.Many2one('medical.guard', ondelete="restrict")
---
+++ cb_medical_guard/models/res_partner.py
@@ -0,0 +1,9 @@
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    guard_journal_id = fields.Many2one(
+        'account.journal'
+    )
