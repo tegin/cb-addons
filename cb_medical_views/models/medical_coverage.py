2401ff88b9243650c4346d41d4dc92f2cf061f90
Enric Tobella <etobella@creublanca.es>
2018-09-18 10:36:50 +0200

    [IMP] display_name

---
+++ cb_medical_views/models/medical_coverage.py
@@ -0,0 +1,15 @@
+from odoo import api, models
+
+
+class MedicalCoverage(models.Model):
+    _inherit = 'medical.coverage'
+
+    @api.multi
+    @api.depends('subscriber_id', 'internal_identifier')
+    def name_get(self):
+        result = []
+        for rec in self:
+            result.append((
+                rec.id,
+                rec.subscriber_id or rec.name or rec.internal_identifier))
+        return result
