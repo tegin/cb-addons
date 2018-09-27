19cc41c4272022fe6393849e27de0dafeba6ba11
Enric Tobella <etobella@creublanca.es>
2018-09-27 12:11:05 +0200

    [ADD] tests on laboratory

---
+++ medical_clinical_laboratory/tests/test_clinical_laboratory.py
@@ -0,0 +1,21 @@
+from odoo.tests.common import TransactionCase
+
+
+class TestClinicalLaboratory(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.patient = self.env['medical.patient'].create({
+            'name': 'Patient'
+        })
+
+    def test_laboratory(self):
+        request = self.env['medical.laboratory.request'].create({
+            'patient_id': self.patient.id,
+        })
+        self.assertEqual(request.laboratory_event_count, 0)
+        event = request.generate()
+        self.assertEqual(request.laboratory_event_count, 1)
+        self.assertEqual(
+            event.id,
+            request.action_view_laboratory_events()['res_id']
+        )
