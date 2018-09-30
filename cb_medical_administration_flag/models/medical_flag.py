c3002885ac189300598f99a54c0edaa240a11e8a
Enric Tobella <etobella@creublanca.es>
2018-09-30 23:59:15 +0200

    [ADD] cb_medical_administration_flag

---
+++ cb_medical_administration_flag/models/medical_flag.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+
+class MedicalFlag(models.Model):
+    # FHIR Entity: Flag (https://www.hl7.org/fhir/flag.html)
+    _inherit = 'medical.flag'
+
+    flag = fields.Char(related='category_id.flag', readonly=True)
