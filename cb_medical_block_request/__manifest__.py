45d5d737d2bc75fdb9674c0fb0f2432f180abb37
Enric Tobella <etobella@creublanca.es>
2018-10-16 15:03:02 +0200

    [ADD] cb_medical_block_request

---
+++ cb_medical_block_request/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Medical Block requests',
+    'version': '11.0.1.0.0',
+    'author': 'Eficent, Creu Blanca, Odoo Community Association (OCA)',
+    'category': 'Medical',
+    'depends': [
+        'cb_medical_pos',
+    ],
+    'data': [
+        'views/medical_request_views.xml',
+        'views/workflow_plan_definition_action.xml',
+    ],
+    'website': 'https://github.com/eficent/cb-addons',
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
