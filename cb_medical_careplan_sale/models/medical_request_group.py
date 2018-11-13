6740b5b00c2734524caf0a95c40848c5a206e666
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-11-13 13:35:04 +0100

    do not pass iternal identifiers during encounter copy()

--- cb_medical_careplan_sale/models/medical_request_group.py
+++ cb_medical_careplan_sale/models/medical_request_group.py
@@ -2,7 +2,8 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models
+from odoo import models, _
+from odoo.exceptions import ValidationError
 
 
 class RequestGroup(models.Model):
@@ -11,8 +12,18 @@ class RequestGroup(models.Model):
     def get_third_party_partner(self):
         if self.third_party_bill:
             request = self.procedure_request_ids
+            if not request:
+                raise ValidationError(_(
+                    'Error trying to determine the Third Party Partner. '
+                    'No Request was found for Request %s' %
+                    request.internal_identifier))
             request.ensure_one()
             procedure = request.procedure_ids
+            if not procedure:
+                raise ValidationError(_(
+                    'Error trying to determine the Third Party Partner. '
+                    'No Procedure was found for Request %s' %
+                    request.internal_identifier))
             procedure.ensure_one()
             if procedure.performer_id.third_party_sequence_id:
                 return procedure.performer_id.id
