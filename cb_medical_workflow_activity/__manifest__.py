9132a9e7493cacc19aa038338c9c3496ce81ce63
Enric Tobella <etobella@creublanca.es>
2018-11-15 18:11:26 +0100

    [ADD] Activity

---
+++ cb_medical_workflow_activity/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'CB Medical Workflow Activity',
+    'version': '11.0.1.0.0',
+    'author': 'Eficent, Creu Blanca, Odoo Community Association (OCA)',
+    'depends': [
+        'medical_workflow',
+    ],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'website': 'https://github.com/OCA/vertical-medical',
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
---
+++ cb_medical_workflow_activity/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import product_template
+from . import product_product
+from . import workflow_activity_definition
