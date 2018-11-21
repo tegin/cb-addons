80730c4a0d6be3672743f71332caa40f0b7ed1c3
Enric Tobella <etobella@creublanca.es>
2018-11-21 16:11:44 +0100

    [IMP] set invoice_group_method on requests in order to invoice correctly

--- cb_medical_authorization/models/medical_coverage_agreement_item.py
+++ cb_medical_authorization/models/medical_coverage_agreement_item.py
@@ -4,14 +4,14 @@ from odoo import models
 class MedicalCoverageAgreementItem(models.Model):
     _inherit = 'medical.coverage.agreement.item'
 
-    def _check_authorization(self, **kwargs):
-        res = super()._check_authorization(**kwargs)
+    def _check_authorization(self, method, **kwargs):
+        res = super()._check_authorization(method, **kwargs)
         res.update({
             'authorization_checked': kwargs.get('authorization_checked', False)
         })
         if (
             not kwargs.get('authorization_checked', False) and
-            self.authorization_method_id.check_required
+            method.check_required
         ):
             res['authorization_status'] = 'pending'
         return res
