73323f4929840bb0ea40cbcc628f5c844d44bdcd
Enric Tobella <etobella@creublanca.es>
2018-11-22 12:05:56 +0100

    fixup! fixup! fixup! [FIX] Move data from group_method to careplan_sale

--- cb_medical_careplan_sale/migrations/11.0.1.0.2/pre-migration.py
+++ cb_medical_careplan_sale/migrations/11.0.1.0.2/pre-migration.py
@@ -3,11 +3,11 @@ def migrate(cr, version):
         "CREATE TABLE invoice_group_method (LIKE"
         "sale_invoice_group_method including all);")
     cr.execute(
-        "INSERT INTO invoice_group_method SELECT * FROM" 
+        "INSERT INTO invoice_group_method SELECT * FROM "
         "sale_invoice_group_method;")
     cr.execute(
-        "UPDATE ir_model_data SET model = 'invoice.group.method'" 
+        "UPDATE ir_model_data SET model = 'invoice.group.method' "
         "WHERE model = 'sale.invoice.group.method';")
     cr.execute(
-        "CREATE SEQUENCE invoice_group_method_id_seq" 
+        "CREATE SEQUENCE invoice_group_method_id_seq "
         "INCREMENT 1 START nextval('sale_invoice_group_method_id_seq');")
