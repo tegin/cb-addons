20c494f930b1bae4225991a0cdb4a8e543120b82
Enric Tobella <etobella@creublanca.es>
2018-11-22 12:48:15 +0100

    fixup! fixup! fixup! fixup! [FIX] Move data from group_method to careplan_sale

--- cb_medical_careplan_sale/migrations/11.0.1.0.2/pre-migration.py
+++ cb_medical_careplan_sale/migrations/11.0.1.0.2/pre-migration.py
@@ -1,6 +1,6 @@
 def migrate(cr, version):
     cr.execute(
-        "CREATE TABLE invoice_group_method (LIKE"
+        "CREATE TABLE invoice_group_method (LIKE "
         "sale_invoice_group_method including all);")
     cr.execute(
         "INSERT INTO invoice_group_method SELECT * FROM "
