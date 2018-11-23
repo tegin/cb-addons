873746bf165effebac162b3fed62bb3c7a3746bb
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-11-23 11:26:33 +0100

    fix script

--- cb_medical_careplan_sale/migrations/11.0.1.0.1/pre-migration.py
+++ cb_medical_careplan_sale/migrations/11.0.1.0.1/pre-migration.py
@@ -8,6 +8,13 @@ def migrate(cr, version):
     cr.execute(
         "UPDATE ir_model_data SET model = 'invoice.group.method' "
         "WHERE model = 'sale.invoice.group.method';")
+
+    cr.execute(
+        "SELECT nextval('sale_invoice_group_method_id_seq')"
+    )
+    next_seq = 1
+    for val in cr.fetchone():
+        next_seq = val
     cr.execute(
         "CREATE SEQUENCE invoice_group_method_id_seq "
-        "INCREMENT 1 START nextval('sale_invoice_group_method_id_seq');")
+        "INCREMENT 1 START %s;",  (next_seq, ))
