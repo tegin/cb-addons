222a4d5b0aa3b5d72127d67d1a227f3f0b27b269
Enric Tobella <etobella@creublanca.es>
2018-11-26 16:00:24 +0100

    [ADD] cb_medical_invocie

---
+++ cb_medical_invoice/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'CB Medical sequence configuration',
+    'version': '11.0.1.0.0',
+    'author': 'Eficent, Creu Blanca, Odoo Community Association (OCA)',
+    'depends': [
+        'cb_medical_pos',
+        'cb_medical_sale_invoice',
+        'cb_medical_sale_invoice_group_method',
+        'pos_validation',
+        'sale',
+    ],
+    'data': [
+        'wizard/medical_encounter_change_partner_views.xml',
+        'views/medical_encounter_views.xml',
+        'views/res_company_views.xml',
+    ],
+    'website': 'https://github.com/OCA/vertical-medical',
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
---
+++ cb_medical_invoice/models/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import account_invoice
+from . import medical_encounter
+from . import res_company
+from . import res_partner
+from . import sale_order
