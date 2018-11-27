d5fddea0e125ad0ab80cf1baaa53e43709d06069
Enric Tobella <etobella@creublanca.es>
2018-11-27 09:15:54 +0100

    fixup! fixup! fixup! fixup! [ADD] cb_medical_invocie

--- cb_medical_invoice/models/__init__.py
+++ cb_medical_invoice/models/__init__.py
@@ -3,6 +3,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from . import account_invoice
+from . import medical_patient
 from . import medical_encounter
 from . import res_company
 from . import res_partner
