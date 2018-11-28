8d20cdb2106bd2ba447af82e9f4132089ac4ebdb
Enric Tobella <etobella@creublanca.es>
2018-11-28 18:32:38 +0100

    [ADD] medical_location type

---
+++ cb_medical_medication/models/res_partner.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    location_type_id = fields.Many2one(
+        'medical.location.type',
+        readonly=True,
+    )
