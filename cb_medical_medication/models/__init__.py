677f703e8eabfda5e7c3a19cfe843ec0f8005ce8
Enric Tobella <etobella@creublanca.es>
2018-11-28 18:32:38 +0100

    [ADD] medical_location type

--- cb_medical_medication/models/__init__.py
+++ cb_medical_medication/models/__init__.py
@@ -2,6 +2,10 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import medical_encounter
+from . import medical_location_type
 from . import medical_medication_item
 from . import medical_medication_request
 from . import product_category
+from . import res_partner
+from . import workflow_activity_definition
+from . import workflow_plan_definition_action
