da2706a3336016ffd5f2b56d76c7e8ef11b47a59
Enric Tobella <etobella@creublanca.es>
2018-12-12 13:50:52 +0100

    [ADD] agreement: Apply percentage to all the agreement

--- cb_medical_financial_coverage_agreement/wizard/medical_agreement_change_prices.py
+++ cb_medical_financial_coverage_agreement/wizard/medical_agreement_change_prices.py
@@ -9,34 +9,20 @@ class MedicalAgreementChangePrices(models.TransientModel):
     _name = 'medical.agreement.change.prices'
 
     difference = fields.Float(
-        string="Indicate the percentage to apply to the selected item(s):",
+        string="Indicate the percentage to apply to the agreement",
     )
 
     @api.multi
     def change_prices(self):
         context = dict(self._context or {})
-        items = self.env['medical.coverage.agreement.item'].browse(context.get(
+        agreements = self.env['medical.coverage.agreement'].browse(context.get(
             'active_ids'))
-        agreement = items[0].coverage_agreement_id
-        list_items = ""
-        count = 0
-        for item in items:
-            item.total_price = item.total_price + ((item.total_price *
-                                                    self.difference) / 100)
-            count += 1
-            if len(items) > count:
-                list_items = list_items + item.display_name + ', '
-            else:
-                list_items = list_items + _("and ") + item.display_name + '.'
-        agreement.message_post(
-            body=_("Prices have been changed by a %s &#037 by %s in %s") %
-                  (self.difference, self.env.user.display_name, list_items),
-        )
-        result = {
-            "type": "ir.actions.act_window",
-            "res_model": "medical.coverage.agreement.item",
-            "domain": [("coverage_agreement_id", "=", agreement.id)],
-            "name": "Agreement Items",
-        }
-        result['views'] = [(False, "tree"), (False, "form")]
-        return result
+        for agreement in agreements:
+            items = agreement.item_ids
+            for item in items:
+                item.total_price = item.total_price + ((item.total_price *
+                                                        self.difference) / 100)
+            agreement.message_post(
+                body=_("Prices have been changed by a %s &#037 by %s") %
+                      (self.difference, self.env.user.display_name),
+            )
