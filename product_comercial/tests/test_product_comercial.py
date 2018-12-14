1541c020393ed11730f8147de863e9a6367b6f37
Enric Tobella <etobella@creublanca.es>
2018-12-14 07:46:11 +0100

    [ADD] product_comercial

---
+++ product_comercial/tests/test_product_comercial.py
@@ -0,0 +1,31 @@
+from odoo.tests.common import TransactionCase
+
+
+class TestProductComercial(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.product_obj = self.env['product.product']
+        self.product = self.product_obj.create({
+            'name': 'Product',
+            'comercial': 'Comercial',
+            'company_id': False,
+            'default_code': 'CODE',
+        })
+
+    def test_name_search(self):
+        search = self.product_obj.name_search('Product')
+        aux = [a[0] for a in search]
+        self.assertIn(self.product.id, aux)
+
+    def test_name_search_comercial(self):
+        aux = [a[0] for a in self.product_obj.name_search('Comercial')]
+        self.assertIn(self.product.id, aux)
+
+    def test_search(self):
+        self.assertIn(self.product, self.product_obj.search([
+            ('name', 'ilike', 'Comercial')
+        ]))
+
+    def test_name(self):
+        self.assertEqual(
+            self.product.display_name, '[CODE] Product (Comercial)')
