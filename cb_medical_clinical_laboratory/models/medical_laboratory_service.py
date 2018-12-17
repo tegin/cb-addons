4b9e01835d1081d5c26added82dbdfc46fcf2878
Enric Tobella <etobella@creublanca.es>
2018-12-17 17:04:02 +0100

    [IMP] Search of laboratory service

--- cb_medical_clinical_laboratory/models/medical_laboratory_service.py
+++ cb_medical_clinical_laboratory/models/medical_laboratory_service.py
@@ -2,13 +2,14 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.osv import expression
 
 
 class MedicalLaboratoryService(models.Model):
     _name = 'medical.laboratory.service'
     _description = 'Laboratory service'
-    _rec_name = 'code'
+    _rec_name = 'name'
 
     code = fields.Char(
         required=True,
@@ -34,6 +35,59 @@ class MedicalLaboratoryService(models.Model):
         'unique_laboratory_code', 'UNIQUE(laboratory_code)',
         'Laboratory Code must be unique'
     )]
+
+    @api.depends('code', 'name')
+    def _compute_display_name(self):
+        return super()._compute_display_name()
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        """Include commercial name in direct name search."""
+        args = expression.normalize_domain(args)
+        for arg in args:
+            if isinstance(arg, (list, tuple)):
+                if arg[0] == 'name' or arg[0] == 'display_name':
+                    index = args.index(arg)
+                    args = (
+                        args[:index] + ['|', ('code', arg[1], arg[2])] +
+                        args[index:]
+                    )
+                    break
+        return super().search(
+            args, offset=offset, limit=limit, order=order, count=count, )
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        """Give preference to commercial names on name search"""
+        if not args:
+            args = []
+        recs = self.search(
+            [('code', operator, name)] + args, limit=limit,
+        )
+        res = recs.name_get()
+        if limit:
+            limit_rest = limit - len(recs)
+        else:  # pragma: no cover
+            # limit can be 0 or None representing infinite
+            limit_rest = limit
+        if limit_rest or not limit:
+            args += [('id', 'not in', recs.ids)]
+            res += super().name_search(
+                name, args=args, operator=operator, limit=limit_rest,
+            )
+        return res
+
+    @api.multi
+    def name_get(self):
+        result = []
+        orig_name = dict(super().name_get())
+        for rec in self:
+            name = orig_name[rec.id]
+            code = rec.code
+            if code:
+                name = '[%s] %s' % (code, name)
+            result.append((rec.id, name))
+        return result
 
 
 class MedicalLaboratoryServicePrice(models.Model):
