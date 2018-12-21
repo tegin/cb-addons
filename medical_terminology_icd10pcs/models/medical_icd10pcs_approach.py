a4c0bdb66a25a7f1966660e949e1126409ed6587
Enric Tobella <etobella@creublanca.es>
2018-12-21 12:23:45 +0100

    [ADD] icd10pcs

---
+++ medical_terminology_icd10pcs/models/medical_icd10pcs_approach.py
@@ -0,0 +1,17 @@
+# Copyright 2018 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class MedicalIcd10pcsApproach(models.Model):
+
+    _name = 'medical.icd10pcs.approach'
+    _description = 'Medical Icd10pcs Approach'
+
+    code = fields.Char(required=True)
+    name = fields.Char(translate=True)
+    section_id = fields.Many2one(
+        'medical.icd10pcs.section',
+        required=True,
+    )
---
+++ medical_terminology_icd10pcs/models/medical_icd10pcs_section.py
@@ -0,0 +1,13 @@
+# Copyright 2018 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class MedicalIcd10pcsSection(models.Model):
+
+    _name = 'medical.icd10pcs.section'
+    _description = 'Medical Icd10pcs Section'
+
+    code = fields.Char(required=True)
+    name = fields.Char(translate=True)
