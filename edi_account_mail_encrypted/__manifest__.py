50dc868ec8dcb0bc61ceb4288ca70aefa559a89a
Enric Tobella <etobella@creublanca.es>
2018-12-24 11:51:40 +0100

    [ADD] invoice integration_email_encrypted

---
+++ edi_account_mail_encrypted/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2018 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Invoice Integration Email Encrypted',
+    'summary': """
+        Send invoices through emails as an integration method""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'website': 'www.creublanca.es',
+    'depends': [
+        'invoice_integration_email',
+    ],
+    'data': [
+        'security/security.xml',
+        'wizards/res_view_value.xml',
+        'wizards/res_encrypt_value.xml',
+        'views/res_partner_view.xml',
+    ],
+    'demo': [
+    ],
+}
---
+++ edi_account_mail_encrypted/models/res_partner.py
@@ -0,0 +1,9 @@
+# Copyright 2018 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    email_integration_password = fields.Char(readonly=True)
