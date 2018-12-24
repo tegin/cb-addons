83fb2071bba0914667304b81a66820c698bdab1c
Enric Tobella <etobella@creublanca.es>
2018-12-24 13:07:58 +0100

    [FIX] Travis

--- web_icon_widget/static/src/js/widget.js
+++ web_icon_widget/static/src/js/widget.js
@@ -1,16 +1,17 @@
 odoo.define('web_icon_widget.widget', function (require) {
-"use strict";
+    "use strict";
 
-var basic_fields = require('web.basic_fields');
-var field_registry = require('web.field_registry');
-var FieldIcon = basic_fields.FieldChar.extend({
-    template: 'FieldIcon',
-    supportedFieldTypes: ['char'],
-
+    var basic_fields = require('web.basic_fields');
+    var field_registry = require('web.field_registry');
+    var FieldIcon = basic_fields.FieldChar.extend({
+        template: 'FieldIcon',
+        supportedFieldTypes: ['char'],
         _renderReadonly: function () {
             // Do Nothing
-},
-_renderEdit: function () {this.$input = this.$el.find('input');},
-});
-field_registry.add('icon', FieldIcon);
+        },
+        _renderEdit: function () {
+            this.$input = this.$el.find('input');
+        },
+    });
+    field_registry.add('icon', FieldIcon);
 });
