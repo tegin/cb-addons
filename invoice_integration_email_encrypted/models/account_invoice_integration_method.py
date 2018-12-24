573d847eff9b3986dafeaf4d4cdcfbac1721571f
Enric Tobella <etobella@creublanca.es>
2018-12-24 11:51:40 +0100

    [ADD] invoice integration_email_encrypted

---
+++ invoice_integration_email_encrypted/models/account_invoice_integration_method.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import base64
+from odoo import models, _
+
+
+class AccountInvoiceIntegrationMethod(models.Model):
+    _name = 'account.invoice.integration.method'
+    _inherit = ['account.invoice.integration.method', 'email.encryptor']
+
+    def email_integration_values(self, invoice):
+        res = super().email_integration_values(invoice)
+        if invoice.partner_id.email_integration_password:
+            password = self._decrypt_value(
+                invoice.partner_id.email_integration_password)
+            value = self.env['mail.template'].render_template(
+                password, invoice._name, invoice.id
+            )
+            res['email_password'] = self._encrypt_value(value)
+        return res
