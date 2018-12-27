459d0131e13cc3fb816b9de53e379fc8fcae9291
Enric Tobella <etobella@creublanca.es>
2018-12-27 17:39:27 +0100

    [ADD] discount on agreement

--- cb_medical_sale_invoice/models/medical_coverage_agreement.py
+++ cb_medical_sale_invoice/models/medical_coverage_agreement.py
@@ -5,3 +5,7 @@ class MedicalCoverageAgreement(models.Model):
     _inherit = 'medical.coverage.agreement'
 
     file_reference = fields.Char()
+    discount = fields.Float(
+        default=0.0,
+        help="General discount applied on the final invoices"
+    )
