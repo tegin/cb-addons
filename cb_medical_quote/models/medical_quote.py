83ad7e66c3db1d0d1ecd2447b56f96612e3867f0
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-12-27 13:16:01 +0100

    [cb_medical_quote] improvemnts [cb_medical_sale_invoice] customizations

--- cb_medical_quote/models/medical_quote.py
+++ cb_medical_quote/models/medical_quote.py
@@ -93,6 +93,25 @@ class MedicalQuote(models.Model):
                        states={'draft': [('readonly', False)]},
                        )
 
+    comment_template1_id = fields.Many2one('base.comment.template',
+                                           string='Top Comment Template')
+    comment_template2_id = fields.Many2one('base.comment.template',
+                                           string='Bottom Comment Template')
+    note1 = fields.Html('Top Comment')
+    note2 = fields.Html('Bottom Comment')
+
+    @api.onchange('comment_template1_id')
+    def _set_note1(self):
+        comment = self.comment_template1_id
+        if comment:
+            self.note1 = comment.get_value()
+
+    @api.onchange('comment_template2_id')
+    def _set_note2(self):
+        comment = self.comment_template2_id
+        if comment:
+            self.note2 = comment.get_value()
+
     @api.model
     def _get_agreements_domain(self):
         return [
@@ -303,10 +322,9 @@ class MedicalQuoteLine(models.Model):
     total_price = fields.Float(
         string='Total price',
         required=True,
-        readonly=True,
     )
     coverage_percentage = fields.Float(
-        string='Coverage %', readonly=True,
+        string='Coverage %',
     )
     coverage_agreement_id = fields.Many2one(
         comodel_name='medical.coverage.agreement',
