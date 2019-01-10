0d96b8d565dad3d34a5e2741586a06e9a29ab4d1
Enric Tobella <etobella@creublanca.es>
2019-01-10 16:07:45 +0100

    [IMP] medical_quotes

---
+++ cb_medical_quote/models/product_template.py
@@ -0,0 +1,11 @@
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    description_quote = fields.Text(
+        'Sale Description', translate=True,
+        help="A description of the Product that you want to communicate "
+             "on your quotes"
+    )
