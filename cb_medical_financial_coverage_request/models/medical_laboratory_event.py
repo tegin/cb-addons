fa75f23adcf7ee6a5946e754fd40fbf1bf0f0576
Enric Tobella <etobella@creublanca.es>
2019-01-16 18:03:38 +0100

    [FIX] Change authorization of laboratory

---
+++ cb_medical_financial_coverage_request/models/medical_laboratory_event.py
@@ -0,0 +1,12 @@
+# Copyright 2017 Creu Blanca
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import models
+
+
+class MedicalLaboratoryEvent(models.Model):
+    _inherit = 'medical.laboratory.event'
+
+    def _change_authorization(self, vals, **kwargs):
+        self.write(vals)
