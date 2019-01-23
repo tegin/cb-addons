22201f786b1c837fd8a2c2335e6272e319c9392d
Enric Tobella <etobella@creublanca.es>
2019-01-23 13:55:41 +0100

    [ADD] web_session_management

---
+++ web_session_management/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Web Session Management',
+    'description': """
+        Session management""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca',
+    'depends': [
+        'base_sparse_field',
+        'auth_session_timeout',
+    ],
+    'data': [
+        'wizards/http_session_wizard.xml',
+        'wizards/res_users_sessions_wizard.xml',
+        'views/http_session_user.xml',
+    ],
+    'demo': [
+    ],
+}
