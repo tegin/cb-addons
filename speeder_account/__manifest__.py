cd54257e2ed88370be5c43781ae5489d55c84883
Enric Tobella <etobella@creublanca.es>
2019-01-28 13:41:11 +0100

    [ADD] speeder

---
+++ speeder_account/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2017 Creu Blanca
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Account speeder",
+    "version": "11.0.1.0.0",
+    "category": "Reporting",
+    "website": "https://github.com/OCA/server-tools",
+    "author": "Creu Blanca, "
+              "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Adds a check digit on sequences",
+    "depends": [
+        "account",
+    ],
+    "data": [
+    ],
+}
---
+++ speeder_account/models/account_move.py
@@ -0,0 +1,20 @@
+from odoo import fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    narration = fields.Text(readonly=True)
+    ref = fields.Char(readonly=True)
+    statement_id = fields.Many2one(readonly=True)
+    journal_id = fields.Many2one(readonly=True)
+    company_id = fields.Many2one(readonly=True)
+    date = fields.Date(readonly=True)
+    user_type_id = fields.Many2one(readonly=True)
+
+
+class AccountPartialReconcile(models.Model):
+    _inherit = "account.partial.reconcile"
+
+    company_id = fields.Many2one(readonly=True)
+    company_currency_id = fields.Many2one(readonly=True)
