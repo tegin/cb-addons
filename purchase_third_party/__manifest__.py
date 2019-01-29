4de2cad08b193d39395d3ddb24f7e4f1b37b335b
Enric Tobella <etobella@creublanca.es>
2019-01-29 15:25:44 +0100

    [ADD] purchase_third_party

---
+++ purchase_third_party/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2019 Creu Blanca
+# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Sale third party invoice',
+    'version': '11.0.1.0.0',
+    'author': 'Eficent, Creu Blanca, Odoo Community Association (OCA)',
+    'website': 'http://github.com/eficent/cb-addons',
+    'summary': 'Creates inter company relations',
+    'sequence': 30,
+    'category': 'Sale',
+    'depends': [
+        'mcfix_purchase',
+    ],
+    'license': 'LGPL-3',
+    'data': [
+        'report/external_layout.xml',
+        'report/purchase_order_templates.xml',
+        'report/purchase_quotation_templates.xml',
+        'data/mail_template_data.xml',
+        'views/purchase_order_views.xml',
+        'views/product_supplierinfo_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
