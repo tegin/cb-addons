14fff9ca235d533c31931e13d7d114dcba23ddf1
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-02-19 12:28:35 +0100

    [ADD] cb_invoice_views

---
+++ cb_invoice_views/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Cb Invoice Views',
+    'summary': """
+        Modify invoice views css""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': ['Creu Blanca', 'Odoo Community Association (OCA)'],
+    'website': 'www.creublanca.es',
+    'depends': [
+        'web'
+    ],
+    'data': [
+        'views/theme_default_templates.xml'
+    ],
+    'demo': [
+    ],
+}
