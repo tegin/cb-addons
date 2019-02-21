4ec7628f5c0b640d4277a63fab0f1ecabf6e6da8
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-02-21 18:14:07 +0100

    [ADD] partner_bank_archive

---
+++ partner_bank_archive/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Partner Bank Archive',
+    'summary': """
+        Allows to archive a partner bank""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'website': 'www.creublanca.es',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/res_partner_bank.xml'
+    ],
+    'demo': [
+    ],
+}
