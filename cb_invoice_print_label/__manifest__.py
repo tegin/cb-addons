4741077fa64488c35b35a39eabef3b77b0f7ca5d
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-03-13 12:43:26 +0100

    fixup! [IMP] QR button added on systray menu

--- cb_invoice_print_label/__manifest__.py
+++ cb_invoice_print_label/__manifest__.py
@@ -3,11 +3,11 @@
 
 {
     'name': 'Cb Invoice Print Label',
-    'description': """
+    'summary': """
         Module that allows you to print labels for referencing invoices""",
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Creu Blanca',
+    'author': 'Creu Blanca, Odoo Community Association (OCA)',
     'website': 'www.creublanca.es',
     'depends': [
         'account',
--- hash_search/static/src/js/hash_search_launcher.js
+++ hash_search/static/src/js/hash_search_launcher.js
@@ -1,20 +1,15 @@
 odoo.define('hash_search.hash_search_launcher', function (require) {
     "use strict";
 
-    var core = require('web.core');
     var SystrayMenu = require('web.SystrayMenu');
-    var Widget = require('web.Widget'); /**
-     * Menu item appended in the systray part of the navbar
-     *
-     * The menu item adds the possibility to scan QR tags.
-     **/
+    var Widget = require('web.Widget');
     var LauncherMenu = Widget.extend({
         template:'hash_search_launcher.view.Menu',
         events: {
             "click": "on_click_find_document",
         },
 
-        on_click_find_document: function(){
+        on_click_find_document: function () {
             var context = {};
             context.default_model = 'hash.search';
             context.default_method ='find_hash';
@@ -28,7 +23,6 @@ odoo.define('hash_search.hash_search_launcher', function (require) {
             });
         },
     });
-
 
     SystrayMenu.Items.push(LauncherMenu);
     return {
