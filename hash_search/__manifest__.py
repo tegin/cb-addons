5ded76c820febbc40c23dee81d2a7c675edb6548
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-03-13 09:51:44 +0100

    [ADD] hash_search

---
+++ hash_search/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Hash Search',
+    'summary': """
+        Summary""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca',
+    'website': 'www.creublanca.es',
+    'depends': [
+        'barcode_action',
+    ],
+    'data': [
+        'wizards/barcode_action.xml',
+    ],
+    'demo': [
+    ],
+}
