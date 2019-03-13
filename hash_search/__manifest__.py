ac7b4845a7280cd976f5d2962522af8809e719ac
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-03-13 11:09:48 +0100

    [IMP] QR button added on systray menu

--- hash_search/__manifest__.py
+++ hash_search/__manifest__.py
@@ -7,14 +7,19 @@
         Summary""",
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Creu Blanca',
+    'author': 'Creu Blanca, Odoo Community Association (OCA)',
     'website': 'www.creublanca.es',
     'depends': [
         'barcode_action',
     ],
     'data': [
+        'security/ir.model.access.csv',
+        'views/assets_backend.xml',
         'wizards/barcode_action.xml',
     ],
     'demo': [
     ],
+    'qweb': [
+        'static/src/xml/hash_search_launcher.xml'
+    ]
 }
