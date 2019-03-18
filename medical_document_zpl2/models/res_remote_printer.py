140401a0ea5f74e8d97b655d3480db642ccf243f
Enric Tobella <etobella@creublanca.es>
2019-03-18 15:03:32 +0100

    [FIX] remote_report_to_printer_label

--- medical_document_zpl2/models/res_remote_printer.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class ResRemotePrinter(models.Model):
-    _inherit = 'res.remote.printer'
-
-    printer_usage = fields.Selection(selection_add=[
-        ('label', 'Label')
-    ])
