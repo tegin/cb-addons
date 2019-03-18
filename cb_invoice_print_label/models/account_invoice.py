535e89688a208a68eb68507b93d672e796dcf1b8
Enric Tobella <etobella@creublanca.es>
2019-03-18 15:04:24 +0100

    [FIX] labels

--- cb_invoice_print_label/models/account_invoice.py
+++ cb_invoice_print_label/models/account_invoice.py
@@ -1,28 +1,11 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-from odoo import models
+from odoo import api, models
 
 
 class AccountInvoice(models.Model):
-    _name = 'account.invoice'
-    _inherit = ['account.invoice', 'hash.search.mixin']
+    _inherit = 'account.invoice'
 
-    def action_print_label(self):
-        self.ensure_one()
-        label_action = self.env.ref(
-            'cb_invoice_print_label.account_invoice_print_label'
-        )
-        content = label_action.render_label(self)
-        behaviour = self.remote.with_context(
-            printer_usage='label'
-        ).get_printer_behaviour()
-        if 'printer' not in behaviour:
-            return False
-        printer = behaviour.pop('printer')
-        printer.with_context(
-            print_report_name='labels_%s_%s' % (self._name, self.id)
-        ).print_document(
-            report=self.env['ir.actions.report'],
-            content=content.encode('ascii'), doc_format='txt'
-        )
-        return True
+    @api.model
+    def _get_hash_button_attrs(self):
+        return {
+            "invisible": [("type", "in", ["out_invoice", "out_refund"])]
+        }
