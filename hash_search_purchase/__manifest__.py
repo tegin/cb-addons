7bf0ec22f473ee69fdfc9562abbaffec22a68547
Enric Tobella <etobella@creublanca.es>
2019-03-19 22:53:42 +0100

    [ADD] purhcase as hash_search

---
+++ hash_search_purchase/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Hash Search Account Purchase',
+    'summary': """
+        Allow to use hash search with purchase orders""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'website': 'www.creublanca.es',
+    'depends': [
+        'purchase',
+        'hash_search',
+    ],
+    'data': [
+        'data/purchase_order_label.xml',
+    ],
+}
---
+++ hash_search_purchase/models/__init__.py
@@ -0,0 +1,2 @@
+from . import purchase_order
+from . import hash_search
