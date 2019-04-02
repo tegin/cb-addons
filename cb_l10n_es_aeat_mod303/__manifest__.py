d727298930bf7ba26217db621ca81b88f46b6e77
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-04-02 17:08:18 +0200

    [11.0][ADD] cb_l10n_es_aeat_mod303

---
+++ cb_l10n_es_aeat_mod303/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Cb L10n Es Aeat Mod303',
+    'summary': """
+        Add report to mod 303""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca, Odoo Community Association (OCA)',
+    'website': 'www.creublanca.es',
+    'depends': [
+        'l10n_es_aeat_mod303'
+    ],
+    'data': [
+        'views/aeat_mod_303report.xml'
+    ],
+}
