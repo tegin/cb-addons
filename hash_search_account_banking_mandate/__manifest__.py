ceebcdfd612b08b9fa55361e76144ed0c5cee3ef
Enric Tobella <etobella@creublanca.es>
2019-04-02 09:52:30 +0200

    [ADD] hash_search_account_banking_mandate

---
+++ hash_search_account_banking_mandate/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Hash Search Account Invoice',
+    'summary': """
+        Allow to use hash search with invoices""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'website': 'www.creublanca.es',
+    'depends': [
+        'account_banking_sepa_direct_debit',
+        'hash_search',
+        'web_qr',
+    ],
+    'data': [
+        'reports/sepa_direct_debit_mandate.xml',
+    ],
+}
---
+++ hash_search_account_banking_mandate/models/__init__.py
@@ -0,0 +1,2 @@
+from . import account_banking_mandate
+from . import hash_search
