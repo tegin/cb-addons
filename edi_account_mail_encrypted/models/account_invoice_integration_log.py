b16e08ac2c526be2833e31d862f8894724c02dd2
Enric Tobella <etobella@creublanca.es>
2019-04-03 19:33:51 +0200

    [ADD] Zip files

--- edi_account_mail_encrypted/models/account_invoice_integration_log.py
+++ edi_account_mail_encrypted/models/account_invoice_integration_log.py
@@ -1,9 +1,11 @@
 # Copyright 2018 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, _
-from odoo.exceptions import UserError
+from odoo import models
+from tempfile import TemporaryDirectory
 import logging
 from io import BytesIO
+import subprocess
+import os
 _logger = logging.getLogger(__name__)
 try:
     from PyPDF2 import PdfFileReader, PdfFileWriter
@@ -22,6 +24,8 @@ class AccountInvoiceIntegrationLog(models.Model):
         result = []
         password = self._decrypt_value(
             self.integration_id.email_password)
+        zip_file = False
+        tmpdir = TemporaryDirectory()
         for name, content, mime in res:
             if mime == 'application/pdf':
                 output_pdf = PdfFileWriter()
@@ -33,5 +37,21 @@ class AccountInvoiceIntegrationLog(models.Model):
                 output_pdf.write(buff)
                 result.append((name, buff.getvalue(), mime))
             else:
-                raise UserError(_('Only PDFs are allowed'))
+                if not zip_file:
+                    zip_file = invoice.number + '.zip'
+                with open(os.path.join(tmpdir.name, name), 'wb') as f:
+                    f.write(content)
+                process = subprocess.Popen(
+                    [
+                        "zip", zip_file, name, "--password", password
+                    ],
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                    cwd=tmpdir.name
+                )
+                process.communicate()
+        if zip_file:
+            buff = BytesIO()
+            with open(os.path.join(tmpdir.name, zip_file), 'rb') as f:
+                buff.write(f.readall())
+            result.append((zip_file, buff.getvalue(), 'application/zip'))
         return result
