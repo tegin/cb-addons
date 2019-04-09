540bb86404bde2d0e68a68184819bc22c8df5b53
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-04-09 11:56:27 +0200

    [11.0][ADD] cb_track_action_picking

---
+++ cb_track_action_picking/models/stock_picking.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+
+    _inherit = 'stock.picking'
+
+    receiver_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Receiver',
+        track_visibility='onchange',
+    )
+
+    @api.multi
+    def button_validate(self):
+        res = super(StockPicking, self).button_validate()
+        self.receiver_id = self.env.user.id
+        return res
