1cde969b4f078119a1407ba2780c9dd50a1a7f08
Enric Tobella <etobella@creublanca.es>
2019-05-09 09:22:27 +0200

    [FIX] Author

--- cb_l10n_es_aeat_mod303/__manifest__.py
+++ cb_l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
         Add report to mod 303""",
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Creu Blanca, Odoo Community Association (OCA)',
+    'author': 'Creu Blanca',
     'website': 'www.creublanca.es',
     'depends': [
         'l10n_es_aeat_mod303'
