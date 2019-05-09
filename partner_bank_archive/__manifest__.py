65f2404a95614b3cdc1e3d5c45c51458d04e06f3
Enric Tobella <etobella@creublanca.es>
2019-05-09 09:22:27 +0200

    [FIX] Author

--- partner_bank_archive/__manifest__.py
+++ partner_bank_archive/__manifest__.py
@@ -7,7 +7,7 @@
         Allows to archive a partner bank""",
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'author': 'Creu Blanca',
     'website': 'www.creublanca.es',
     'depends': [
         'account',
