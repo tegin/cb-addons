c5485d3c106ea486e0348771b4442a269a3bc2f8
Enric Tobella <etobella@creublanca.es>
2019-05-09 09:22:27 +0200

    [FIX] Author

--- cb_sale_report_invoice/__manifest__.py
+++ cb_sale_report_invoice/__manifest__.py
@@ -7,7 +7,7 @@
         Sale Report invoice modification for CB""",
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'author': 'Creu Blanca',
     'website': 'www.creublanca.es',
     'depends': [
         'sale',
