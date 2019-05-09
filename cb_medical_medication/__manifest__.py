2272beed0a4663e6fdb8851d13083e3b01209fef
Enric Tobella <etobella@creublanca.es>
2019-05-09 09:22:27 +0200

    [FIX] Author

--- cb_medical_medication/__manifest__.py
+++ cb_medical_medication/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'CB Medical sequence configuration',
     'version': '11.0.1.0.0',
-    'author': 'Eficent, Creu Blanca, Odoo Community Association (OCA)',
+    'author': 'Eficent, Creu Blanca',
     'depends': [
         'cb_medical_careplan_sale',
         'mrp',
