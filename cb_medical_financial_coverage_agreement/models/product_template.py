d74887f86aed0c4f190d79f27efa1b7cf85d4ecd
Enric Tobella <etobella@creublanca.es>
2019-05-13 14:40:30 +0200

    [IMP] Fix category no update on lines

--- cb_medical_financial_coverage_agreement/models/product_template.py
+++ cb_medical_financial_coverage_agreement/models/product_template.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -10,3 +10,16 @@ class ProductTemplate(models.Model):
     include_zero_sales = fields.Boolean(
         default=False
     )
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.multi
+    def write(self, vals):
+        res = super().write(vals)
+        if 'categ_id' in vals:
+            self.env['medical.coverage.agreement.item'].search([
+                ('product_id', 'in', self.ids)
+            ]).write({'categ_id': vals['categ_id']})
+        return res
