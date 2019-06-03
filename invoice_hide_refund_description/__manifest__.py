268331366741ee6d3990720f96c75c215cf67355
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-06-03 15:54:49 +0200

    [12.0][MIG] invoice_hide_refund_description

--- invoice_hide_refund_description/__manifest__.py
+++ invoice_hide_refund_description/__manifest__.py
@@ -6,13 +6,9 @@
     'summary': """
         Module that allows the user not to record the
         description when refunding an invoice""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Creu Blanca',
     'website': 'www.creublanca.es',
     'depends': ['account'],
-    'data': [
-    ],
-    'demo': [
-    ],
 }
