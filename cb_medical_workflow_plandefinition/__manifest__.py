2c6b48f82e11bbca451d38f37b71283a321c7784
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-06-04 11:49:27 +0200

    [12.0][MIG] cb_medical_workflow_plandefinition

--- cb_medical_workflow_plandefinition/__manifest__.py
+++ cb_medical_workflow_plandefinition/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Medical Plan Definition',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Eficent, Creu Blanca',
     'depends': [
         'medical_clinical_request_group',
