37f6bc43fd6fa5d48ce26f6603dfd0ba9d3e7ee5
Enric Tobella <etobella@creublanca.es>
2019-06-06 14:32:46 +0200

    [ADD] document quick access label

---
+++ document_quick_access_label/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Document Quick Access Label',
+    'summary': """
+        Allows to print labels from Document Quick Access records""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/server-ux',
+    'depends': [
+        'document_quick_access',
+        'printer_zpl2',
+        'remote_report_to_printer_label',
+    ],
+    'data': [
+        'views/document_quick_access_rule.xml',
+    ],
+}
