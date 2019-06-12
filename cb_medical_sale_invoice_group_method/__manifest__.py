078c7175cf03bc0801cb5665eb67471e581c741e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-06-12 17:10:36 +0200

    [12.0][MIG] cb_medical_sale_invoice_group_method

--- cb_medical_sale_invoice_group_method/__manifest__.py
+++ cb_medical_sale_invoice_group_method/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Medical Sale Invoice Group Method',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Eficent, Creu Blanca',
     'category': 'Medical',
     'depends': [
