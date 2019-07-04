130119e9af5685af38fc8af124851a99019f6170
Enric Tobella <etobella@creublanca.es>
2019-07-04 15:58:14 +0200

    [ADD] create_encounter functions

--- cb_medical_careplan_sale/models/medical_careplan.py
+++ cb_medical_careplan_sale/models/medical_careplan.py
@@ -2,7 +2,8 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, models
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
 
 SO_OPEN = ['draft']
 
@@ -19,3 +20,41 @@ class MedicalCareplan(models.Model):
         if self.sub_payor_id:
             return self.sub_payor_id.id
         return self.payor_id.id
+
+    def _add_request_group(
+        self, service=False, qty=1, order_by=False, authorization_number=False,
+        performer=False, **kwargs
+    ):
+        self.ensure_one()
+        if not service:
+            raise ValidationError(_('Service is required'))
+        if isinstance(service, int):
+            service = self.env['product.product'].browse(service)
+        if not order_by:
+            order_by = self.env['res.partner']
+        elif isinstance(order_by, int):
+            order_by = self.env['res.partner'].browse(order_by)
+        if not performer:
+            performer = self.env['res.partner']
+        elif isinstance(performer, int):
+            performer = self.env['res.partner'].browse(performer)
+        call = self.env['medical.careplan.add.plan.definition'].with_context(
+            default_careplan_id=self.id,
+        ).create({
+            'order_by_id': order_by.id or False,
+            'authorization_number': authorization_number,
+            'qty': qty,
+        })
+        agreement_line = self.env['medical.coverage.agreement.item'].search([
+            ('product_id', '=', service.id),
+            ('coverage_agreement_id', 'in', call.agreement_ids.ids),
+            ('plan_definition_id', '!=', False),
+        ], limit=1)
+        agreement_line.ensure_one()
+        write_vals = {'agreement_line_id': agreement_line.id}
+        if agreement_line.plan_definition_id.performer_required:
+            if isinstance(performer, int):
+                performer = self.env['res.partner'].browse(performer)
+            write_vals['performer_id'] = performer.id
+        call.write(write_vals)
+        return call._run()
--- cb_medical_careplan_sale/models/medical_encounter.py
+++ cb_medical_careplan_sale/models/medical_encounter.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class MedicalEncounter(models.Model):
@@ -130,3 +131,82 @@ class MedicalEncounter(models.Model):
             result['views'] = [(False, 'form')]
             result['res_id'] = self.sale_order_ids.id
         return result
+
+    @api.model
+    def _create_encounter(
+            self, patient=False, patient_vals=False, center=False, **kwargs
+    ):
+        encounter = super()._create_encounter(
+            patient, patient_vals, center, **kwargs
+        )
+        careplan_data = kwargs.get('careplan_data') or []
+        for careplan_vals in careplan_data:
+            careplan_kwargs = kwargs.copy()
+            careplan_kwargs.update(careplan_vals)
+            encounter._add_careplan(**careplan_kwargs)
+        return encounter
+
+    def _add_careplan(
+        self, payor=False, sub_payor=False, coverage_template=False,
+        subscriber_id=False, coverage=False, service=False, order_by=False,
+        performer=False, authorization_number=False, qty=1,
+        subscriber_magnetic_str=False, **kwargs
+    ):
+        if coverage:
+            if isinstance(coverage, int):
+                coverage = self.env['medical.coverage'].browse(coverage)
+            if self.patient_id != coverage.patient_id:
+                raise ValidationError(_('Patient must be the same'))
+            if not payor:
+                payor = coverage.coverage_template_id.payor_id
+            if not coverage_template:
+                coverage_template = coverage.coverage_template_id
+        else:
+            coverage = self.env['medical.coverage']
+        if not payor:
+            raise ValidationError(_('Payor is required'))
+        if isinstance(payor, int):
+            payor = self.env['res.partner'].browse(payor)
+        if not coverage_template:
+            raise ValidationError(_('Coverage template is required'))
+        if isinstance(coverage_template, int):
+            coverage_template = self.env[
+                'medical.coverage.template'].browse(coverage_template)
+        if not service:
+            raise ValidationError(_('Service is required'))
+        if isinstance(service, int):
+            service = self.env['product.product'].browse(service)
+        if not sub_payor:
+            sub_payor = self.env['res.partner']
+        elif isinstance(sub_payor, int):
+            sub_payor = self.env['res.partner'].browse(sub_payor)
+        if not order_by:
+            order_by = self.env['res.partner']
+        elif isinstance(order_by, int):
+            order_by = self.env['res.partner'].browse(order_by)
+        if not performer:
+            performer = self.env['res.partner']
+        elif isinstance(performer, int):
+            performer = self.env['res.partner'].browse(performer)
+        self.ensure_one()
+        careplan = self.env[
+            'medical.encounter.add.careplan'
+        ].with_context(
+            default_encounter_id=self.id,
+        ).create({
+            'payor_id': payor.id or coverage.payor_id.id or False,
+            'coverage_template_id': coverage_template.id or False,
+            'sub_payor_id': sub_payor.id or False,
+            'subscriber_id': subscriber_id,
+            'subscriber_magnetic_str': subscriber_magnetic_str,
+            'coverage_id': coverage.id or False,
+        }).with_context(
+            default_order_by_id=order_by.id or False,
+            default_service_id=service.id or False
+        ).run()
+        careplan._add_request_group(
+            service=service, qty=qty, order_by=order_by,
+            authorization_number=authorization_number,
+            performer=performer, **kwargs
+        )
+        return careplan
