594afc13966d2e261746eacc5d1c287e7bb95aba
Enric Tobella <etobella@creublanca.es>
2019-07-31 11:08:53 +0200

    [FIX] cb_hr_views: Error on creation of events from leaves

--- cb_hr_views/models/hr_holidays.py
+++ cb_hr_views/models/hr_holidays.py
@@ -1,10 +1,17 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class HrHolidays(models.Model):
 
     _name = 'hr.holidays'
     _inherit = ['mail.thread', 'mail.activity.mixin', 'hr.holidays']
+
+    @api.multi
+    def _prepare_holidays_meeting_values(self):
+        result = super()._prepare_holidays_meeting_values()
+        if not result.get('partner_ids', False):
+            result['partner_ids'] = [(4, self.employee_id.partner_id.id)]
+        return result
