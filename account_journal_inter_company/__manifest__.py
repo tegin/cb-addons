8195438251245d3cde7b6059731d26cdfc06b96e
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- account_journal_inter_company/__manifest__.py
+++ account_journal_inter_company/__manifest__.py
@@ -3,21 +3,21 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Intercompany journal',
-    'version': '11.0.1.0.0',
-    'author': 'Eficent, Creu Blanca',
-    'website': 'http://github.com/eficent/cb-addons',
-    'summary': 'Creates inter company relations',
-    'sequence': 30,
-    'category': 'Accounting',
-    'depends': ['mcfix_account'],
-    'license': 'LGPL-3',
-    'data': [
-        'security/ir.model.access.csv',
-        'views/res_company_views.xml',
-        'views/res_inter_company_views.xml',
+    "name": "Intercompany journal",
+    "version": "11.0.1.0.0",
+    "author": "Eficent, Creu Blanca",
+    "website": "http://github.com/eficent/cb-addons",
+    "summary": "Creates inter company relations",
+    "sequence": 30,
+    "category": "Accounting",
+    "depends": ["mcfix_account"],
+    "license": "LGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_company_views.xml",
+        "views/res_inter_company_views.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
