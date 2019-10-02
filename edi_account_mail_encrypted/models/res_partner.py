24e98ecb1e58d7cb98ed0ecb8c33d927fb239707
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- edi_account_mail_encrypted/models/res_partner.py
+++ edi_account_mail_encrypted/models/res_partner.py
@@ -4,6 +4,6 @@ from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     email_integration_password = fields.Char(readonly=True)
