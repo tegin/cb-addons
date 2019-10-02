f8c89f8ddc943ae2e3934b1f89ad95e5f8ea1b16
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- pos_manual_order/__manifest__.py
+++ pos_manual_order/__manifest__.py
@@ -2,19 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Pos Manual Order',
-    'summary': """
+    "name": "Pos Manual Order",
+    "summary": """
         Add Orders manually on a PoS Session""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'depends': [
-        'point_of_sale',
-    ],
-    'data': [
-        'wizards/pos_session_add_order.xml',
-        'views/pos_session.xml',
-    ],
-    'demo': [
-    ],
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "depends": ["point_of_sale"],
+    "data": ["wizards/pos_session_add_order.xml", "views/pos_session.xml"],
+    "demo": [],
 }
--- pos_manual_order/wizards/pos_session_add_order.py
+++ pos_manual_order/wizards/pos_session_add_order.py
@@ -6,81 +6,72 @@ from odoo import api, fields, models
 
 class PosSessionAddOrder(models.TransientModel):
 
-    _name = 'pos.session.add.order'
+    _name = "pos.session.add.order"
 
-    session_id = fields.Many2one(
-        'pos.session',
-        required=True,
-    )
-    product_id = fields.Many2one(
-        'product.product',
-        required=True,
-    )
-    fiscal_position_id = fields.Many2one(
-        'account.fiscal.position',
-    )
-    partner_id = fields.Many2one(
-        'res.partner',
-    )
-    price = fields.Monetary(
-        currency_field='currency_id',
-        required=True,
-    )
+    session_id = fields.Many2one("pos.session", required=True)
+    product_id = fields.Many2one("product.product", required=True)
+    fiscal_position_id = fields.Many2one("account.fiscal.position")
+    partner_id = fields.Many2one("res.partner")
+    price = fields.Monetary(currency_field="currency_id", required=True)
     discount = fields.Float()
-    qty = fields.Integer(
-        required=True,
-        default=1,
-    )
+    qty = fields.Integer(required=True, default=1)
     amount_total = fields.Monetary(
-        currency_field='currency_id',
-        compute='_compute_amount_total'
+        currency_field="currency_id", compute="_compute_amount_total"
     )
     currency_id = fields.Many2one(
-        'res.currency',
-        related='session_id.config_id.company_id.currency_id',
+        "res.currency",
+        related="session_id.config_id.company_id.currency_id",
         readonly=True,
     )
-    journal_id = fields.Many2one(
-        'account.journal',
-        required=True,
-    )
+    journal_id = fields.Many2one("account.journal", required=True)
     journal_ids = fields.Many2many(
-        'account.journal',
-        compute='_compute_journal_ids'
+        "account.journal", compute="_compute_journal_ids"
     )
 
-    @api.depends('session_id')
+    @api.depends("session_id")
     def _compute_journal_ids(self):
         for rec in self:
-            rec.journal_ids = rec.session_id.statement_ids.mapped('journal_id')
+            rec.journal_ids = rec.session_id.statement_ids.mapped("journal_id")
 
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def _onchange_product(self):
         self.price = self.product_id.lst_price
 
     @api.depends(
-        'product_id', 'session_id.config_id.company_id', 'partner_id',
-        'price', 'discount',
+        "product_id",
+        "session_id.config_id.company_id",
+        "partner_id",
+        "price",
+        "discount",
     )
     def _compute_amount_total(self):
         for rec in self:
             taxes = rec.product_id.taxes_id.filtered(
-                lambda t: t.company_id.id == rec.session_id.company_id.id)
+                lambda t: t.company_id.id == rec.session_id.company_id.id
+            )
             fiscal_position_id = rec.fiscal_position_id
             if fiscal_position_id:
                 taxes = fiscal_position_id.map_tax(
-                    taxes, rec.product_id, rec.partner_id)
+                    taxes, rec.product_id, rec.partner_id
+                )
             price = rec.price * (1 - (rec.discount or 0.0) / 100.0)
             taxes = taxes.compute_all(
-                price, rec.session_id.config_id.company_id.currency_id,
-                rec.qty, product=rec.product_id,
-                partner=rec.partner_id or False
+                price,
+                rec.session_id.config_id.company_id.currency_id,
+                rec.qty,
+                product=rec.product_id,
+                partner=rec.partner_id or False,
             )
-            rec.amount_total = taxes['total_included']
+            rec.amount_total = taxes["total_included"]
 
     @api.multi
     def run(self):
         return self.session_id._add_manual_order(
-            self.product_id, self.qty, self.price, self.discount,
-            self.partner_id, self.fiscal_position_id, self.journal_id
+            self.product_id,
+            self.qty,
+            self.price,
+            self.discount,
+            self.partner_id,
+            self.fiscal_position_id,
+            self.journal_id,
         )
