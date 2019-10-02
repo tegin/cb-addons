2f9172f1291c05d648afadd0d4042c1214474595
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- safe_box/models/safe_box_group.py
+++ safe_box/models/safe_box_group.py
@@ -6,53 +6,50 @@ from odoo import api, fields, models
 
 
 class SafeBoxGroup(models.Model):
-    _name = 'safe.box.group'
-    _description = 'Safe box group'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _name = "safe.box.group"
+    _description = "Safe box group"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
 
     code = fields.Char(required=True)
     name = fields.Char(required=True)
     account_ids = fields.One2many(
-        comodel_name='account.account',
-        inverse_name='safe_box_group_id',
-        string='Accounts'
+        comodel_name="account.account",
+        inverse_name="safe_box_group_id",
+        string="Accounts",
     )
     safe_box_ids = fields.One2many(
-        comodel_name='safe.box',
-        inverse_name='safe_box_group_id',
-        string='Safe boxes'
+        comodel_name="safe.box",
+        inverse_name="safe_box_group_id",
+        string="Safe boxes",
     )
     coin_ids = fields.One2many(
-        comodel_name='safe.box.coin',
-        inverse_name='safe_box_group_id',
-        string='Coins'
+        comodel_name="safe.box.coin",
+        inverse_name="safe_box_group_id",
+        string="Coins",
     )
-    currency_id = fields.Many2one(
-        'res.currency',
-        required=True,
-    )
-    sequence_id = fields.Many2one('ir.sequence', string='Entry Sequence', )
+    currency_id = fields.Many2one("res.currency", required=True)
+    sequence_id = fields.Many2one("ir.sequence", string="Entry Sequence")
 
     @api.model
     def _create_sequence(self, vals):
         """ Create new no_gap entry sequence for every new Safe Box Group"""
         seq = {
-            'name': vals['name'],
-            'implementation': 'no_gap',
-            'prefix': vals['code'],
-            'padding': 4,
-            'number_increment': 1,
-            'use_date_range': True,
+            "name": vals["name"],
+            "implementation": "no_gap",
+            "prefix": vals["code"],
+            "padding": 4,
+            "number_increment": 1,
+            "use_date_range": True,
         }
-        seq = self.env['ir.sequence'].create(seq)
+        seq = self.env["ir.sequence"].create(seq)
         seq_date_range = seq._get_current_sequence()
         seq_date_range.number_next = 1
         return seq
 
     @api.model
     def create(self, vals):
-        if not vals.get('sequence_id'):
-            vals.update({'sequence_id': self.sudo()._create_sequence(vals).id})
+        if not vals.get("sequence_id"):
+            vals.update({"sequence_id": self.sudo()._create_sequence(vals).id})
         return super(SafeBoxGroup, self).create(vals)
 
     @api.multi
--- safe_box/wizard/wizard_safe_box_count.py
+++ safe_box/wizard/wizard_safe_box_count.py
@@ -7,77 +7,65 @@ from odoo.tools import float_compare
 
 
 class WizardSafeBoxCount(models.TransientModel):
-    _name = 'wizard.safe.box.count'
+    _name = "wizard.safe.box.count"
 
-    safe_box_group_id = fields.Many2one(
-        'safe.box.group',
-        required=True,
-    )
+    safe_box_group_id = fields.Many2one("safe.box.group", required=True)
     safe_box_id = fields.Many2one(
-        'safe.box',
+        "safe.box",
         domain="[('safe_box_group_id', '=', safe_box_group_id)]",
         required=True,
     )
     coin_ids = fields.One2many(
-        'wizard.safe.box.count.coin',
-        inverse_name='safe_box_count_id',
+        "wizard.safe.box.count.coin",
+        inverse_name="safe_box_count_id",
         domain="[('safe_box_coin_id', 'in', safe_box_coin_ids)]",
-        string='Coins',
+        string="Coins",
     )
     safe_box_coin_ids = fields.Many2many(
-        'safe.box.coin',
-        related='safe_box_id.coin_ids',
-        readonly=True,
+        "safe.box.coin", related="safe_box_id.coin_ids", readonly=True
     )
-    state = fields.Selection([
-        ('equal', 'Equal'),
-        ('different', 'Different')
-    ], default=False)
+    state = fields.Selection(
+        [("equal", "Equal"), ("different", "Different")], default=False
+    )
     status = fields.Char()
 
-    @api.onchange('safe_box_id')
+    @api.onchange("safe_box_id")
     def _onchange_safe_box_id(self):
         for record in self:
             record.coin_ids = [
-                {'safe_box_coin_id': coin.id}
-                for coin in record.safe_box_id.coin_ids]
+                {"safe_box_coin_id": coin.id}
+                for coin in record.safe_box_id.coin_ids
+            ]
 
-    @api.onchange('coin_ids')
+    @api.onchange("coin_ids")
     def validate(self):
         self.ensure_one()
         self.safe_box_id.recompute_amount()
-        amount = sum(self.coin_ids.mapped('amount'))
-        if float_compare(
-                amount, self.safe_box_id.amount, precision_digits=6
-        ) == 0:
-            self.state = 'equal'
-            self.status = _('Cash Box amount is correct')
+        amount = sum(self.coin_ids.mapped("amount"))
+        if (
+            float_compare(amount, self.safe_box_id.amount, precision_digits=6)
+            == 0
+        ):
+            self.state = "equal"
+            self.status = _("Cash Box amount is correct")
         else:
-            self.state = 'different'
-            self.status = _('Cash Box amount is different')
+            self.state = "different"
+            self.status = _("Cash Box amount is different")
 
 
 class WizardSafeBoxCountCoin(models.TransientModel):
-    _name = 'wizard.safe.box.count.coin'
+    _name = "wizard.safe.box.count.coin"
 
     safe_box_count_id = fields.Many2one(
-        'wizard.safe.box.count.coin',
-        readonly=True,
+        "wizard.safe.box.count.coin", readonly=True
     )
     safe_box_coin_id = fields.Many2one(
-        'safe.box.coin',
-        required=True,
-        string='Coin'
+        "safe.box.coin", required=True, string="Coin"
     )
-    value = fields.Integer(
-        required=True,
-        default=0
-    )
-    amount = fields.Float(
-        compute='_compute_amount'
-    )
+    value = fields.Integer(required=True, default=0)
+    amount = fields.Float(compute="_compute_amount")
 
-    @api.depends('value', 'safe_box_coin_id')
+    @api.depends("value", "safe_box_coin_id")
     def _compute_amount(self):
         for record in self:
             record.amount = record.safe_box_coin_id.rate * record.value
