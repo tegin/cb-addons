30e8a4ba639f8050a68ee2ee06c38745b93f6400
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- pos_close_approval/models/pos_config.py
+++ pos_close_approval/models/pos_config.py
@@ -6,11 +6,11 @@ from odoo import fields, models
 
 
 class PosConfig(models.Model):
-    _inherit = 'pos.config'
+    _inherit = "pos.config"
 
     requires_approval = fields.Boolean(default=False)
 
     def closed_states(self):
         res = super(PosConfig, self).closed_states()
-        res.append('pending_approval')
+        res.append("pending_approval")
         return res
