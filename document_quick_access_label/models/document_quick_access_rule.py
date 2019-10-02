982bfc2cbaa66d6c53619dc21c8ac6497986435f
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- document_quick_access_label/models/document_quick_access_rule.py
+++ document_quick_access_label/models/document_quick_access_rule.py
@@ -5,16 +5,13 @@ from odoo import fields, models
 
 
 class DocumentQuickAccessRule(models.Model):
-    _inherit = 'document.quick.access.rule'
+    _inherit = "document.quick.access.rule"
 
-    label_id = fields.Many2one(
-        'printing.label.zpl2',
-        'ZPL2 Label'
-    )
+    label_id = fields.Many2one("printing.label.zpl2", "ZPL2 Label")
     icon = fields.Char()
     label_name = fields.Char()
     label_attrs = fields.Char()
 
     def _get_button_attrs(self):
         self.ensure_one()
-        return self.label_attrs or '{}'
+        return self.label_attrs or "{}"
