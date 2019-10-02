97fad3e8be48e2f5de702e363d78a6967d43b9f6
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- remote_report_to_printer_label/__manifest__.py
+++ remote_report_to_printer_label/__manifest__.py
@@ -2,14 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Remote Report To Printer Label',
-    'summary': """
+    "name": "Remote Report To Printer Label",
+    "summary": """
         Adds label selection to remote printer""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'remote_report_to_printer',
-    ],
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["remote_report_to_printer"],
 }
