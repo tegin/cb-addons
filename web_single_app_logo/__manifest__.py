4709ccfd5d92869e9e01d9a95060f614948c137a
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- web_single_app_logo/__manifest__.py
+++ web_single_app_logo/__manifest__.py
@@ -8,12 +8,9 @@
     "license": "AGPL-3",
     "category": "Website",
     "summary": "Allows to define a single app logo",
-    "depends": [
-        "base_setup",
-        "web",
-    ],
+    "depends": ["base_setup", "web"],
     "data": [
-        'views/res_config_settings_views.xml',
-        'views/template_views.xml',
+        "views/res_config_settings_views.xml",
+        "views/template_views.xml",
     ],
 }
