c10ca7043cf09ff2572732b5227fe2b27f891400
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- stock_request_template/__manifest__.py
+++ stock_request_template/__manifest__.py
@@ -2,20 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Stock Request Template',
-    'summary': """
+    "name": "Stock Request Template",
+    "summary": """
         Create TEmplates for Stock Request Orders""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'stock_request',
-    ],
-    'data': [
-        'security/ir.model.access.csv',
-        'wizards/stock_request_order_template.xml',
-        'views/stock_request_template.xml',
-        'views/stock_request_order.xml',
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "www.creublanca.es",
+    "depends": ["stock_request"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/stock_request_order_template.xml",
+        "views/stock_request_template.xml",
+        "views/stock_request_order.xml",
     ],
 }
