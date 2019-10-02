00abe97d73233b6c3cef5cad091d82d4a93beeac
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- pos_safe_box/__manifest__.py
+++ pos_safe_box/__manifest__.py
@@ -3,24 +3,21 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Safe Box with PoS',
-    'version': '11.0.1.0.0',
-    'author': 'Eficent, Creu Blanca',
-    'depends': [
-        'safe_box',
-        'pos_close_approval',
+    "name": "Safe Box with PoS",
+    "version": "11.0.1.0.0",
+    "author": "Eficent, Creu Blanca",
+    "depends": ["safe_box", "pos_close_approval"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_sequence_data.xml",
+        "views/pos_session_validation_views.xml",
+        "views/pos_session_views.xml",
+        "views/pos_config_views.xml",
+        "views/safe_box_group_views.xml",
+        "views/safe_box_coin_views.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/ir_sequence_data.xml',
-        'views/pos_session_validation_views.xml',
-        'views/pos_session_views.xml',
-        'views/pos_config_views.xml',
-        'views/safe_box_group_views.xml',
-        'views/safe_box_coin_views.xml',
-    ],
-    'website': 'https://github.com/eficent/cb-addons',
-    'license': 'LGPL-3',
-    'installable': True,
-    'auto_install': False,
+    "website": "https://github.com/eficent/cb-addons",
+    "license": "LGPL-3",
+    "installable": True,
+    "auto_install": False,
 }
