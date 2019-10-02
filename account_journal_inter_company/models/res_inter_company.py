e568519704800fdafdba5f3045f79b902468d336
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- account_journal_inter_company/models/res_inter_company.py
+++ account_journal_inter_company/models/res_inter_company.py
@@ -7,34 +7,28 @@ from odoo.exceptions import ValidationError
 
 
 class ResInterCompany(models.Model):
-    _name = 'res.inter.company'
+    _name = "res.inter.company"
 
-    company_id = fields.Many2one(
-        comodel_name='res.company',
-        required=True,
-    )
+    company_id = fields.Many2one(comodel_name="res.company", required=True)
     journal_id = fields.Many2one(
-        comodel_name='account.journal',
+        comodel_name="account.journal",
         required=True,
         domain="[('company_id', '=', company_id)]",
     )
-    inter_company_id = fields.Many2one(
-        'res.inter.company',
-        ondelete='cascade'
-    )
+    inter_company_id = fields.Many2one("res.inter.company", ondelete="cascade")
     related_company_id = fields.Many2one(
-        comodel_name='res.company',
-        string='Related company',
-        related='inter_company_id.company_id',
+        comodel_name="res.company",
+        string="Related company",
+        related="inter_company_id.company_id",
         domain="[('id', '!=', company_id)]",
         required=True,
         store=True,
         index=True,
     )
     related_journal_id = fields.Many2one(
-        comodel_name='account.journal',
-        string='Related journal',
-        related='inter_company_id.journal_id',
+        comodel_name="account.journal",
+        string="Related journal",
+        related="inter_company_id.journal_id",
         domain="[('company_id', '=', related_company_id)]",
         required=True,
     )
@@ -42,22 +36,34 @@ class ResInterCompany(models.Model):
     @api.model
     def create(self, vals):
         res = super(ResInterCompany, self).create(vals)
-        related = super(ResInterCompany, self).create({
-            'company_id': vals.get('related_company_id'),
-            'journal_id': vals.get('related_journal_id'),
-            'related_company_id': vals.get('company_id'),
-            'related_journal_id': vals.get('journal_id'),
-            'inter_company_id': res.id,
-        })
+        related = super(ResInterCompany, self).create(
+            {
+                "company_id": vals.get("related_company_id"),
+                "journal_id": vals.get("related_journal_id"),
+                "related_company_id": vals.get("company_id"),
+                "related_journal_id": vals.get("journal_id"),
+                "inter_company_id": res.id,
+            }
+        )
         res.inter_company_id = related
         return res
 
-    @api.constrains('company_id', 'related_company_id')
+    @api.constrains("company_id", "related_company_id")
     def _check_company(self):
         for record in self:
-            if self.search_count([
-                ('company_id', '=', record.company_id.id),
-                ('related_company_id', '=', record.related_company_id.id)
-            ]) > 1:
-                raise ValidationError(_(
-                    'Only one record per company is allowed'))
+            if (
+                self.search_count(
+                    [
+                        ("company_id", "=", record.company_id.id),
+                        (
+                            "related_company_id",
+                            "=",
+                            record.related_company_id.id,
+                        ),
+                    ]
+                )
+                > 1
+            ):
+                raise ValidationError(
+                    _("Only one record per company is allowed")
+                )
