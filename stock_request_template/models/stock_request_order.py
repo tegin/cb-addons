5eb0941b82041233659aafb8f840c9165014a3a1
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- stock_request_template/models/stock_request_order.py
+++ stock_request_template/models/stock_request_order.py
@@ -5,28 +5,26 @@ from odoo import api, models
 
 
 class StockRequestOrder(models.Model):
-    _inherit = 'stock.request.order'
+    _inherit = "stock.request.order"
 
     @api.multi
     def _apply_template(self, template):
         for line in template.template_line_ids:
             vals = self._template_line_vals(template, line)
-            self.env['stock.request'].create(
-                vals
-            )
+            self.env["stock.request"].create(vals)
         return {}
 
     def _template_line_vals(self, template, line):
         return {
-            'warehouse_id': template.warehouse_id.id,
-            'location_id': template.location_id.id,
-            'company_id': template.company_id.id,
-            'picking_policy': self.picking_policy,
-            'expected_date': self.expected_date,
-            'requested_by': self.requested_by.id,
-            'procurement_group_id': self.procurement_group_id.id,
-            'product_id': line.product_id.id,
-            'product_uom_qty': line.product_uom_qty,
-            'product_uom_id': line.product_uom_id.id,
-            'order_id': self.id,
+            "warehouse_id": template.warehouse_id.id,
+            "location_id": template.location_id.id,
+            "company_id": template.company_id.id,
+            "picking_policy": self.picking_policy,
+            "expected_date": self.expected_date,
+            "requested_by": self.requested_by.id,
+            "procurement_group_id": self.procurement_group_id.id,
+            "product_id": line.product_id.id,
+            "product_uom_qty": line.product_uom_qty,
+            "product_uom_id": line.product_uom_id.id,
+            "order_id": self.id,
         }
