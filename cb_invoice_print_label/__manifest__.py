0b559bc364e21d73be87cc9caf8602b0ebd7cbae
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- cb_invoice_print_label/__manifest__.py
+++ cb_invoice_print_label/__manifest__.py
@@ -2,22 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cb Invoice Print Label',
-    'summary': """
+    "name": "Cb Invoice Print Label",
+    "summary": """
         Module that allows you to print labels for referencing invoices""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'remote_report_to_printer_label',
-        'printer_zpl2',
-        'hash_search_account_invoice',
-        'hash_search_document_scanner',
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": [
+        "remote_report_to_printer_label",
+        "printer_zpl2",
+        "hash_search_account_invoice",
+        "hash_search_document_scanner",
     ],
-    'data': [
-        'data/account_invoice_label.xml',
-    ],
-    'demo': [
-    ],
+    "data": ["data/account_invoice_label.xml"],
+    "demo": [],
 }
