ff4a5f8abaf320332317b83b8cabc61ad0d6ae68
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- l10n_es_facturae_sftp/models/account_invoice_integration_method.py
+++ l10n_es_facturae_sftp/models/account_invoice_integration_method.py
@@ -5,13 +5,11 @@ from odoo import models, _
 
 
 class AccountInvoiceIntegrationMethod(models.Model):
-    _inherit = 'account.invoice.integration.method'
+    _inherit = "account.invoice.integration.method"
 
     def integration_values(self, invoice):
         res = super().integration_values(invoice)
-        if self == self.env.ref(
-            'l10n_es_facturae_sftp.integration_sftp'
-        ):
+        if self == self.env.ref("l10n_es_facturae_sftp.integration_sftp"):
             res.update(self.sftp_integration_values(invoice))
         return res
 
@@ -25,13 +23,15 @@ class AccountInvoiceIntegrationMethod(models.Model):
         if action.attachment:
             attachment = action.retrieve_attachment(invoice)
         if not attachment:
-            fname = _('Invoice %s') % invoice.number
-            attachment = self.env['ir.attachment'].create({
-                'name': fname,
-                'datas': base64.b64encode(content),
-                'datas_fname': fname,
-                'res_model': 'account.invoice',
-                'res_id': invoice.id,
-                'mimetype': 'application/pdf'
-            })
-        return {'attachment_id': attachment.id}
+            fname = _("Invoice %s") % invoice.number
+            attachment = self.env["ir.attachment"].create(
+                {
+                    "name": fname,
+                    "datas": base64.b64encode(content),
+                    "datas_fname": fname,
+                    "res_model": "account.invoice",
+                    "res_id": invoice.id,
+                    "mimetype": "application/pdf",
+                }
+            )
+        return {"attachment_id": attachment.id}
