a67071925671053b50941988e873b3be06d79a51
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- account_journal_inter_company/models/res_company.py
+++ account_journal_inter_company/models/res_company.py
@@ -6,20 +6,18 @@ from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     inter_company_ids = fields.One2many(
-        comodel_name='res.inter.company',
-        inverse_name='company_id'
+        comodel_name="res.inter.company", inverse_name="company_id"
     )
 
     related_company_ids = fields.Many2many(
-        comodel_name='res.company',
-        compute='_compute_related_company_ids'
+        comodel_name="res.company", compute="_compute_related_company_ids"
     )
 
     def _compute_related_company_ids(self):
         for record in self:
             record.related_company_ids = record.inter_company_ids.mapped(
-                'related_company_id'
+                "related_company_id"
             )
--- account_journal_inter_company/tests/test_intercompany.py
+++ account_journal_inter_company/tests/test_intercompany.py
@@ -7,16 +7,18 @@ from odoo.exceptions import ValidationError
 
 
 class TestInterCompanyJournal(common.TestInterCompany):
-
     def test_intercompany(self):
         self.create_inter_company(self.company_1, self.company_2)
         self.assertTrue(self.company_1.inter_company_ids)
         self.assertTrue(self.company_2.inter_company_ids)
         self.assertEqual(
             self.company_1.inter_company_ids.inter_company_id,
-            self.company_2.inter_company_ids)
-        self.assertTrue(self.company_1.related_company_ids.filtered(
-            lambda r: r.id == self.company_2.id
-        ))
+            self.company_2.inter_company_ids,
+        )
+        self.assertTrue(
+            self.company_1.related_company_ids.filtered(
+                lambda r: r.id == self.company_2.id
+            )
+        )
         with self.assertRaises(ValidationError):
             self.create_inter_company(self.company_1, self.company_2)
