d5adeb6a4320c1379f57817ee3fca3e322614ae2
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- cb_stock_location_warning/__manifest__.py
+++ cb_stock_location_warning/__manifest__.py
@@ -2,19 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cb Stock Location Warning',
-    'summary': """
+    "name": "Cb Stock Location Warning",
+    "summary": """
         Add warnings to stock locations""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'purchase',
-        'stock',
-    ],
-    'data': [
-        'views/purchase_order_templates.xml',
-        'views/stock_location.xml',
-    ],
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["purchase", "stock"],
+    "data": ["views/purchase_order_templates.xml", "views/stock_location.xml"],
 }
--- cb_stock_location_warning/models/stock_location.py
+++ cb_stock_location_warning/models/stock_location.py
@@ -6,6 +6,6 @@ from odoo import fields, models
 
 class StockLocation(models.Model):
 
-    _inherit = 'stock.location'
+    _inherit = "stock.location"
 
-    warning = fields.Text(string='Warning')
+    warning = fields.Text(string="Warning")
