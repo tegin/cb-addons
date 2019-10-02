b3ad5fcf32964cedc468b0ab3b32ef6451e8272a
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- pos_close_approval/__manifest__.py
+++ pos_close_approval/__manifest__.py
@@ -3,25 +3,23 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'PoS Close approval',
-    'version': '11.0.1.0.0',
-    'category': 'Reporting',
-    'website': 'https://github.com/eficent/cb-addons',
-    'author': 'Creu Blanca, Eficent',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Adds integration information',
-    'depends': [
-        'pos_multiple_sessions',
+    "name": "PoS Close approval",
+    "version": "11.0.1.0.0",
+    "category": "Reporting",
+    "website": "https://github.com/eficent/cb-addons",
+    "author": "Creu Blanca, Eficent",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Adds integration information",
+    "depends": ["pos_multiple_sessions"],
+    "data": [
+        "wizard/cash_box_journal_in.xml",
+        "wizard/cash_box_journal_out.xml",
+        "wizard/bank_statement_account.xml",
+        "views/pos_config_views.xml",
+        "views/pos_session_views.xml",
+        "views/bank_statement_views.xml",
+        "security/point_of_sale_security.xml",
     ],
-    'data': [
-        'wizard/cash_box_journal_in.xml',
-        'wizard/cash_box_journal_out.xml',
-        'wizard/bank_statement_account.xml',
-        'views/pos_config_views.xml',
-        'views/pos_session_views.xml',
-        'views/bank_statement_views.xml',
-        'security/point_of_sale_security.xml',
-    ]
 }
