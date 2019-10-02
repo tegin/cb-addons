13c6932cc8c1d6e3e7998cb0905f7bc9592fe057
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- cb_l10n_es_aeat_mod303/__manifest__.py
+++ cb_l10n_es_aeat_mod303/__manifest__.py
@@ -2,17 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cb L10n Es Aeat Mod303',
-    'summary': """
+    "name": "Cb L10n Es Aeat Mod303",
+    "summary": """
         Add report to mod 303""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'l10n_es_aeat_mod303'
-    ],
-    'data': [
-        'views/aeat_mod_303report.xml'
-    ],
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["l10n_es_aeat_mod303"],
+    "data": ["views/aeat_mod_303report.xml"],
 }
