b284aa3b070eea43f99be20a0f180ab9452ff007
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- sequence_parser/__manifest__.py
+++ sequence_parser/__manifest__.py
@@ -3,21 +3,17 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Sequence parser',
-    'version': '11.0.1.0.0',
-    'author': 'Eficent, Creu Blanca',
-    'website': 'http://github.com/eficent/cb-addons',
-    'summary': 'Returns the sequence on a tuple',
-    'sequence': 30,
-    'category': 'Sale',
-    'depends': [
-        'base',
-        'sequence_check_digit',
-    ],
-    'license': 'LGPL-3',
-    'data': [
-    ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "name": "Sequence parser",
+    "version": "11.0.1.0.0",
+    "author": "Eficent, Creu Blanca",
+    "website": "http://github.com/eficent/cb-addons",
+    "summary": "Returns the sequence on a tuple",
+    "sequence": 30,
+    "category": "Sale",
+    "depends": ["base", "sequence_check_digit"],
+    "license": "LGPL-3",
+    "data": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
