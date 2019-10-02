0ee746640748478d4a11b878e405e11ea3cdd00a
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- cb_sale_report_invoice/__manifest__.py
+++ cb_sale_report_invoice/__manifest__.py
@@ -2,17 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cb Sale Report Invoice',
-    'summary': """
+    "name": "Cb Sale Report Invoice",
+    "summary": """
         Sale Report invoice modification for CB""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'sale',
-    ],
-    'data': [
-        'views/report_invoice.xml',
-    ],
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["sale"],
+    "data": ["views/report_invoice.xml"],
 }
