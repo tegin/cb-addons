4ceb4f7e88fa0455fd477ec8e411bd6fcbe962d9
Enric Tobella <etobella@creublanca.es>
2019-10-02 09:47:20 +0200

    [ADD] pre-commit hooks

--- safe_box/models/account_move.py
+++ safe_box/models/account_move.py
@@ -7,20 +7,20 @@ from odoo.exceptions import ValidationError
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
     safe_box_move_id = fields.Many2one(
-        comodel_name='safe.box.move',
+        comodel_name="safe.box.move",
         readonly=True,
-        string='Move',
+        string="Move",
         copy=False,
-        delete='restrict'
+        delete="restrict",
     )
     safe_box_group_id = fields.Many2one(
-        comodel_name='safe.box.group',
-        related='safe_box_move_id.safe_box_group_id',
+        comodel_name="safe.box.group",
+        related="safe_box_move_id.safe_box_group_id",
         store=True,
-        readonly=True
+        readonly=True,
     )
 
     @api.multi
@@ -28,11 +28,13 @@ class AccountMove(models.Model):
         for move in self:
             safe_box_group = move.safe_box_move_id.safe_box_group_id
             if move.line_ids.filtered(
-                lambda r: r.account_id.safe_box_group_id and
-                    r.account_id.safe_box_group_id != safe_box_group
+                lambda r: r.account_id.safe_box_group_id
+                and r.account_id.safe_box_group_id != safe_box_group
             ):
-                raise ValidationError(_(
-                    'Accounts with a related safe box must be under safe box '
-                    'moves'
-                ))
+                raise ValidationError(
+                    _(
+                        "Accounts with a related safe box must be under safe box "
+                        "moves"
+                    )
+                )
         super(AccountMove, self)._post_validate()
