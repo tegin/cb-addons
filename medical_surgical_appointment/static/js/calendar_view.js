eb3e19ccc5d8f437b4906b70f0dd20ffd9fef492
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-10-03 09:07:13 +0200

    [11.0][FIX] Surgical appointment fixes

--- medical_surgical_appointment/static/js/calendar_view.js
+++ medical_surgical_appointment/static/js/calendar_view.js
@@ -2,16 +2,13 @@ odoo.define('medical_surgical_appointment.CalendarView', function (require) {
 "use strict";
 
 var CalendarView = require('web.CalendarView');
-var session = require("web.session");
 
 var CalendarViewNoCreate = CalendarView.include({
     init: function (viewInfo, params) {
         this._super.apply(this, arguments);
         var arch = viewInfo.arch;
         var attrs = arch.attrs;
-        console.log('hola')
-        console.log(session.user_context)
-        if (session.user_context['no_create_calendar']){
+        if (params.context['no_drag']){
             this.loadParams.creatable = false;
         }
     },
