016ee718fd508f406b5b597e0ec3d5745057b6ae
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-10-23 16:58:56 +0200

    add account_asset_management_depreciation_percentage

---
+++ account_asset_management_depreciation_percentage/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Asset Management Depreciation Percentage",
+    "version": "11.0.1.0.1",
+    "depends": [
+        "account_asset_management",
+    ],
+    "conflicts": ['l10n_es_account_asset'],
+    "author": "Eficent",
+    "license": "AGPL-3",
+    "website": "https://www.eficent.com/",
+    "category": "Accounting & Finance",
+    "data": [
+        "views/account_asset_profile_views.xml",
+        "views/account_asset_views.xml",
+    ],
+    "installable": True,
+}
