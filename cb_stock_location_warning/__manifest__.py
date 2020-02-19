b9a1c55251f9c1bff08974be474d8756b220cca1
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-19 12:18:11 +0100

    [12.0][MIG] cb_stock_location_warning

--- cb_stock_location_warning/__manifest__.py
+++ cb_stock_location_warning/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Cb Stock Location Warning",
     "summary": """
         Add warnings to stock locations""",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
