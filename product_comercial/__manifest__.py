e1f6645393f0704a94700ecc5964dfcb61ac9ffb
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-19 12:19:12 +0100

    [12.0][MIG] product_comercial

--- product_comercial/__manifest__.py
+++ product_comercial/__manifest__.py
@@ -5,11 +5,10 @@
     "name": "Product Comercial",
     "summary": """
         Add Comercial field to product""",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
     "depends": ["product"],
     "data": ["views/product_template.xml"],
-    "demo": [],
 }
