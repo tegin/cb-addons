b03a02ca952f480c77dc3c78964bb71a67c4e5f5
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-19 12:49:39 +0100

    [12.0][MIG] account_journal_inter_company

--- account_journal_inter_company/__manifest__.py
+++ account_journal_inter_company/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Intercompany journal",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, Creu Blanca",
     "website": "http://github.com/eficent/cb-addons",
     "summary": "Creates inter company relations",
