5fc0d973274c740f971bb13a7c26a837c6694262
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-19 12:12:38 +0100

    [12.0][MIG] cb_invoice_views

--- cb_invoice_views/__manifest__.py
+++ cb_invoice_views/__manifest__.py
@@ -5,11 +5,10 @@
     "name": "Cb Invoice Views",
     "summary": """
         Modify invoice views css""",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
-    "depends": ["web"],
+    "depends": ["web", "account", "medical_administration"],
     "data": ["views/theme_default_templates.xml"],
-    "demo": [],
 }
