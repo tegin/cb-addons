e0f89eec24971f25b08d99c05e471eda5d40d3f0
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-19 12:46:24 +0100

    [12.0][MIG] cb_sale_report_invoice

--- cb_sale_report_invoice/__manifest__.py
+++ cb_sale_report_invoice/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Cb Sale Report Invoice",
     "summary": """
         Sale Report invoice modification for CB""",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
