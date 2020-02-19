5e447d6df7d87988571c93d0aa81adeee9deae36
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-19 12:36:41 +0100

    [12.0][MIG] pos_manual_order

--- pos_manual_order/__manifest__.py
+++ pos_manual_order/__manifest__.py
@@ -5,10 +5,9 @@
     "name": "Pos Manual Order",
     "summary": """
         Add Orders manually on a PoS Session""",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "depends": ["point_of_sale"],
     "data": ["wizards/pos_session_add_order.xml", "views/pos_session.xml"],
-    "demo": [],
 }
