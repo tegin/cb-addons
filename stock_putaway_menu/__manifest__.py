2fed34e21541b78f5bd58b8fd4624e8161de458b
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-20 12:53:09 +0100

    [12.0][MIG] stock_putaway_menu

--- stock_putaway_menu/__manifest__.py
+++ stock_putaway_menu/__manifest__.py
@@ -5,11 +5,10 @@
     "name": "Stock Putaway Menu",
     "summary": """
         Show menu for putaway strategies""",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "www.creublanca.es",
     "depends": ["stock"],
     "data": ["views/product_putaway.xml"],
-    "demo": [],
 }
