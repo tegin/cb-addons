d8dbda5e5aeb83c8126ef95f0c7c98851bb243db
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-24 14:03:59 +0100

    [12.0][MIG] cb_quality_report

--- cb_quality_report/__manifest__.py
+++ cb_quality_report/__manifest__.py
@@ -2,18 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cb Quality Report',
-    'description': """
+    "name": "Cb Quality Report",
+    "description": """
         QC Inspections Report""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'quality_control',
-    ],
-    'data': [
-        'report/questionary.xml',
-        'report/qc_inspection_report.xml',
-    ],
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["quality_control"],
+    "data": ["report/questionary.xml", "report/qc_inspection_report.xml"],
 }
