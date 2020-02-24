6e9b813db497e907ab9cc505bd4e8dc66dd73f9e
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-24 17:01:19 +0100

    [12.0][MIG] mgmt system

--- cb_mgmtsystem_issue/__manifest__.py
+++ cb_mgmtsystem_issue/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Cb Mgmtsystem Issue',
     'summary': """
         Managemente System Issues""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Creu Blanca',
     'website': 'www.creublanca.es',
