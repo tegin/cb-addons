5526862301d9980066cf2765f7b6730dfe17d52f
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-27 11:47:14 +0100

    [12.0][MIG] document_quick_access_field

--- document_quick_access_field/__manifest__.py
+++ document_quick_access_field/__manifest__.py
@@ -5,11 +5,10 @@
     "name": "Document Quick Access Code",
     "summary": """
         Allow to access a document throught a field""",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "www.creublanca.es",
     "depends": ["document_quick_access"],
     "data": ["views/document_quick_access_rule.xml"],
-    "demo": [],
 }
