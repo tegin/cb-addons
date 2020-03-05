f4e275a4c0bd3e7689eaf49c686adc539df95885
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-05 18:49:35 +0100

    [12.0][MIG] l10n_es_mis_report_expand

--- l10n_es_mis_report_expand/__manifest__.py
+++ l10n_es_mis_report_expand/__manifest__.py
@@ -2,21 +2,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'L10n Es Mis Report Expand',
-    'summary': """
+    "name": "L10n Es Mis Report Expand",
+    "summary": """
         Plantillas extendidas MIS Builder para informes contables españoles""",
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Eficent, Creu Blanca',
-    'website': 'https://github.com/OCA/l10n-spain',
-    'depends': [
-        "l10n_es_mis_report",
-    ],
-    'data': [
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Eficent, Creu Blanca",
+    "website": "https://github.com/OCA/l10n-spain",
+    "depends": ["l10n_es_mis_report"],
+    "data": [
         "data/mis_report_styles.xml",
         "data/mis_report_balance_sme.xml",
-        'data/mis_report_pyg_sme.xml',
-    ],
-    'demo': [
+        "data/mis_report_pyg_sme.xml",
     ],
 }
