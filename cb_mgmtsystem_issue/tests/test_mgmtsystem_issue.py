478bce37718e8c1dcdaf8b64579dd4a59beb4369
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-10 18:29:18 +0100

    [12.0] black

--- cb_mgmtsystem_issue/tests/test_mgmtsystem_issue.py
+++ cb_mgmtsystem_issue/tests/test_mgmtsystem_issue.py
@@ -5,29 +5,30 @@ from odoo.tests.common import TransactionCase
 
 
 class TestMgmtsystemIssue(TransactionCase):
-
     def setUp(self):
         super().setUp()
-        self.partner_id = self.env['res.partner'].create({'name': 'Partner'})
-        self.origin = self.env['mgmtsystem.nonconformity.origin'].create({
-            'name': 'origin',
-        })
-        self.issue = self.env['mgmtsystem.quality.issue'].create({
-            'name': 'Title',
-            'description': 'Description',
-            'partner_id': self.partner_id.id,
-            'responsible_user_id': self.env.uid,
-            'manager_user_id': self.env.uid,
-            'origin': [(4, self.origin.id)]
-        })
+        self.partner_id = self.env["res.partner"].create({"name": "Partner"})
+        self.origin = self.env["mgmtsystem.nonconformity.origin"].create(
+            {"name": "origin"}
+        )
+        self.issue = self.env["mgmtsystem.quality.issue"].create(
+            {
+                "name": "Title",
+                "description": "Description",
+                "partner_id": self.partner_id.id,
+                "responsible_user_id": self.env.uid,
+                "manager_user_id": self.env.uid,
+                "origin": [(4, self.origin.id)],
+            }
+        )
 
     def test_mgmtsystem_issue(self):
         self.issue.to_accepted()
-        self.assertEqual(self.issue.state, 'ok')
+        self.assertEqual(self.issue.state, "ok")
 
         self.issue.back_to_pending()
-        self.assertEqual(self.issue.state, 'pending')
+        self.assertEqual(self.issue.state, "pending")
 
         action = self.issue.to_nonconformity()
-        self.assertEqual(self.issue.state, 'no_ok')
-        self.assertEqual(self.issue.non_conformity_id.id, action['res_id'])
+        self.assertEqual(self.issue.state, "no_ok")
+        self.assertEqual(self.issue.non_conformity_id.id, action["res_id"])
