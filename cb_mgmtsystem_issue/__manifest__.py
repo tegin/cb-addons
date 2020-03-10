fd3a09e2048ce1abb5d20c890069da701ce6c279
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-10 18:29:18 +0100

    [12.0] black

--- cb_mgmtsystem_issue/__manifest__.py
+++ cb_mgmtsystem_issue/__manifest__.py
@@ -2,20 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cb Mgmtsystem Issue',
-    'summary': """
+    "name": "Cb Mgmtsystem Issue",
+    "summary": """
         Managemente System Issues""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'mgmtsystem_nonconformity',
-    ],
-    'data': [
-        'data/mgmtsystem_sequence.xml',
-        'security/ir.model.access.csv',
-        'security/msmsystem_security.xml',
-        'views/mgmtsystem_quality_issue.xml',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["mgmtsystem_nonconformity"],
+    "data": [
+        "data/mgmtsystem_sequence.xml",
+        "security/ir.model.access.csv",
+        "security/msmsystem_security.xml",
+        "views/mgmtsystem_quality_issue.xml",
     ],
 }
