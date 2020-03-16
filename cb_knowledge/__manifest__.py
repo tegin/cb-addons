d24eeae171c93539ebf437b260bac4c75ccbfec9
Enric Tobella <etobella@creublanca.es>
2020-03-16 22:22:18 +0100

    [ADD] cb_knowledge

---
+++ cb_knowledge/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Cb Knowledge",
+    "summary": """
+        Knowledge for CB""",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "www.creublanca.es",
+    "depends": [
+        "document_page_reference",
+        "document_page_approval",
+        "report_qr",
+    ],
+    "data": ["views/report_document_page.xml"],
+}
