a5cf7a9f75d939e713aed2c65e18cde03e7e5684
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-03-24 12:09:34 +0100

    [12.0][ADD] document_quick_access_remote_printer

---
+++ document_quick_access_remote_printer/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Document Quick Access Remote Printer',
+    'description': """
+        def _get_printer""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca',
+    'depends': [
+        'remote_report_to_printer_label'
+    ],
+}
---
+++ document_quick_access_remote_printer/models/base.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class Base(models.AbstractModel):
+    _inherit = 'base'
+
+    def _get_printer(self):
+        behaviour = self.remote.with_context(
+            printer_usage='label'
+        ).get_printer_behaviour()
+        if 'printer' not in behaviour:
+            return False
+        printer = behaviour.pop('printer')
+        return printer
