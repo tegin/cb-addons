9279af589a8e545692caeccd715a7c70f4b87879
Enric Tobella <etobella@creublanca.es>
2020-03-30 13:25:08 +0200

    [IMP] pos_manual_order: Fix + Tests

--- pos_manual_order/__manifest__.py
+++ pos_manual_order/__manifest__.py
@@ -8,6 +8,6 @@
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
-    "depends": ["point_of_sale"],
+    "depends": ["mcfix_point_of_sale"],
     "data": ["wizards/pos_session_add_order.xml", "views/pos_session.xml"],
 }
