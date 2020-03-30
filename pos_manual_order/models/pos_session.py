36dfab8dc1fec23edff6236154ab06784d97d1a7
Enric Tobella <etobella@creublanca.es>
2020-03-30 13:25:08 +0200

    [IMP] pos_manual_order: Fix + Tests

--- pos_manual_order/models/pos_session.py
+++ pos_manual_order/models/pos_session.py
@@ -12,22 +12,19 @@ class PosSession(models.Model):
         self, product, qty, price, discount, partner, fiscal_position
     ):
         return {
-            "company_id": self.company_id.id,
+            "company_id": self.config_id.company_id.id,
             "session_id": self.id,
+            "pricelist_id": self.config_id.pricelist_id.id,
             "partner_id": partner.id or False,
             "fiscal_position_id": fiscal_position.id or False,
-            "lines": [
-                (0, 0, r)
-                for r in self._get_manual_order_line_vals(
-                    product, qty, price, discount
-                )
-            ],
         }
 
-    def _get_manual_order_line_vals(self, product, qty, price, discount):
+    def _get_manual_order_line_vals(
+        self, product, qty, price, discount, order
+    ):
         return [
             {
-                "company_id": self.config_id.company_id.id,
+                "order_id": order.id,
                 "name": self.env["ir.sequence"].next_by_code("pos.order.line"),
                 "product_id": product.id,
                 "price_unit": price,
@@ -60,7 +57,21 @@ class PosSession(models.Model):
         vals = self._get_manual_order_vals(
             product, qty, price, discount, partner, fiscal_position
         )
-        order = self.env["pos.order"].create(vals)
+        order_object = self.env["pos.order"].new(vals)
+        order_object._onchange_amount_all()
+        order = self.env["pos.order"].create(
+            order_object._convert_to_write(order_object._cache)
+        )
+        for line_vals in self._get_manual_order_line_vals(
+            product, qty, price, discount, order
+        ):
+            line = self.env["pos.order.line"].new(line_vals)
+            line._onchange_qty()
+            self.env["pos.order.line"].create(
+                line._convert_to_write(line._cache)
+            )
+            order._onchange_amount_all()
+
         data = self._get_manual_order_payment_data(order, journal)
         order.add_payment(data)
         if order.test_paid():
