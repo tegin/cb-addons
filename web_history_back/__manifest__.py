4113778554da1eb5027d0dce79a831d0c8c34b04
Enric Tobella <etobella@creublanca.es>
2020-03-31 16:45:48 +0200

    [ADD] web_history_back

---
+++ web_history_back/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Web History Back",
+    "description": """
+        Add history back""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["web"],
+    "data": ["views/templates.xml"],
+    "demo": ["demo/res_partner.xml"],
+}
