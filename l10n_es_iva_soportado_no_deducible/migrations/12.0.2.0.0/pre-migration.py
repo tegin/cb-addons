8e02cf3e758b419171b2ffecc0d386f6702eb70b
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2020-04-06 17:52:08 +0200

    * add l10n_es_iva_incluido * migrate l10n_es_iva_soportado_no_deducible   now that https://github.com/odoo/odoo/pull/48956 has been merged   in order to avoid duplicities.

---
+++ l10n_es_iva_soportado_no_deducible/migrations/12.0.2.0.0/pre-migration.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+xmlid_renames = [
+    ('l10n_es_iva_soportado_no_deducible.account_tax_template_p_iva10_nd',
+     'l10n_es.account_tax_template_p_iva10_nd'),
+    ('l10n_es_iva_soportado_no_deducible.account_tax_template_p_iva4_nd',
+     'l10n_es.account_tax_template_p_iva4_nd'),
+]
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_xmlids(env.cr, xmlid_renames)
