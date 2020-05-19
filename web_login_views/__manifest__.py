48bed66a6b031ad56c08ea7dfa8e7403c86f0f98
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-05-19 10:35:00 +0200

    [12.0][ADD] web_login_views

---
+++ web_login_views/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Web Login Views",
+    "summary": """
+        Web Login Views""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "www.creublanca.es",
+    "depends": ["web"],
+    "data": ["views/webclient_templates.xml"],
+}
