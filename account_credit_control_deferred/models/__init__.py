f5275da498031307b936e2d11d8c4ff74841aef9
Enric Tobella <etobella@creublanca.es>
2020-09-15 18:43:31 +0200

    [ADD] account_credit_control_deferred

---
+++ account_credit_control_deferred/models/__init__.py
@@ -0,0 +1,6 @@
+from . import credit_control_run
+from . import credit_control_policy_level
+from . import credit_control_line
+from . import credit_control_communication
+from . import mail_mail
+from . import res_partner
