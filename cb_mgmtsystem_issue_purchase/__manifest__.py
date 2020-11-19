33784175d917bbd02950fee84fb06ebf702b63ae
Enric Tobella <etobella@creublanca.es>
2020-11-19 15:46:11 +0100

    [ADD] cb_mgmtsystem_issue_purchase

---
+++ cb_mgmtsystem_issue_purchase/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Cb Management system Issue Purchase",
+    "summary": """Cb Management system Issue Purchase""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["cb_mgmtsystem_issue", "purchase"],
+    "data": ["views/purchase_order.xml"],
+}
