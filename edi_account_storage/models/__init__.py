e14dc7f3eddee18f033ebab7bfbfe0c9476023ef
Alba Riera <alba.riera@creublanca.es>
2021-05-11 18:41:39 +0200

    [MIG] l10n_es_facturae_storage: add storage_backend logic

---
+++ edi_account_storage/models/__init__.py
@@ -0,0 +1,3 @@
+from . import res_partner
+from . import account_invoice_integration_log
+from . import account_invoice_integration_method
---
+++ edi_account_storage/tests/__init__.py
@@ -0,0 +1,2 @@
+from . import test_storage_integration
+from . import test_storage_partner
