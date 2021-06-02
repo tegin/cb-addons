ccb9cb1139836574e8ccc30f30847580f68cd221
Enric Tobella <etobella@creublanca.es>
2021-06-02 16:19:28 +0200

    [IMP] githubactions: Copy logic from OCA

--- sale_third_party/__manifest__.py
+++ sale_third_party/__manifest__.py
@@ -11,7 +11,7 @@
     "sequence": 30,
     "category": "Sale",
     "depends": ["mcfix_sale", "multicompany_property_account"],
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "data": [
         "views/sale_order_views.xml",
         "views/res_company_views.xml",
