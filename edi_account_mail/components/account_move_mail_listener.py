ae0afb121948cc07c6b97b9a76ab680dbce0803f
Enric Tobella <etobella@creublanca.es>
2021-06-22 17:58:32 +0200

    [MIG] edi_account_mail: Migration to 13.0

---
+++ edi_account_mail/components/account_move_mail_listener.py
@@ -0,0 +1,42 @@
+# Copyright 2020 Creu Blanca
+# @author: Enric Tobella
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import Component
+
+
+class AccountMoveMailListener(Component):
+    _name = "account.move.mail.listener"
+    _inherit = "base.event.listener"
+    _apply_on = ["account.move"]
+
+    def _get_backend(self, record):
+        return self.env.ref("edi_account_mail.mail_backend")
+
+    def _get_exchange_record_vals(self, record):
+        return {
+            "model": record._name,
+            "res_id": record.id,
+        }
+
+    def on_post_account_move(self, records):
+        for record in records:
+            partner = record.partner_id
+            if record.type not in ["out_invoice", "out_refund"]:
+                continue
+            if not partner.send_invoice_by_mail:
+                continue
+            backend = self._get_backend(record)
+            if not backend:
+                continue
+            exchange_type = "account_move_mail"
+            if record._has_exchange_record(exchange_type, backend):
+                continue
+            exchange_record = backend.create_record(
+                exchange_type, self._get_exchange_record_vals(record)
+            )
+            backend.exchange_generate(exchange_record)
+            backend.exchange_send(exchange_record)
+
+    def on_generate_account_edi(self, records):
+        return self.on_post_account_move(records)
--- edi_account_mail/models/res_partner.py
+++ edi_account_mail/models/res_partner.py
@@ -6,7 +6,9 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
+    send_invoice_by_mail = fields.Boolean()
     email_integration = fields.Char()
     invoice_report_email_id = fields.Many2one(
         "ir.actions.report", domain=[("model", "=", "account.invoice")]
     )
+    # invoice_mail_exchange_type_id = fields.Many2one("edi.exchange.type")
