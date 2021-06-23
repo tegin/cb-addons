bc7ebe8dbda53805339edd32e4b911d899b2efa0
Olga Marco <olga.marco@creublanca.es>
2021-06-23 18:01:08 +0200

    [MIG] cb_helpdesk: Migration to 13.0

--- cb_helpdesk/__manifest__.py
+++ cb_helpdesk/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Cb Helpdesk",
     "summary": """
         Helpdesk from OCA with some modifications""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "www.creublanca.es",
