e3da5bd1ec882acc80aac205298dbb267c70ad92
Enric Tobella <etobella@creublanca.es>
2021-06-28 17:06:24 +0200

    [MIG] remote_report_to_printer_label: Migration to 13.0

--- remote_report_to_printer_label/__manifest__.py
+++ remote_report_to_printer_label/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Remote Report To Printer Label",
     "summary": """
         Adds label selection to remote printer""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
