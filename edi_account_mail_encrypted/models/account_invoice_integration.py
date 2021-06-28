b0e8dd508c74ccba068809ae6a9027f77f042df9
Enric Tobella <etobella@creublanca.es>
2021-06-28 12:53:43 +0200

    [MIG] edi_account_mail_encrypted: Migration to 13.0

--- edi_account_mail_encrypted/models/account_invoice_integration.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2018 Creu Blanca
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
-
-
-class AccountInvoiceIntegration(models.Model):
-    _inherit = "account.invoice.integration"
-
-    email_password = fields.Char()
