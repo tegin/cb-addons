12903ea1b31f0da587932458fc856aa5fadb87e1
Enric Tobella <etobella@creublanca.es>
2021-06-28 12:53:43 +0200

    [MIG] edi_account_mail_encrypted: Migration to 13.0

--- edi_account_mail_encrypted/tests/test_invoice_integration.py
+++ edi_account_mail_encrypted/tests/test_invoice_integration.py
@@ -1,159 +1,36 @@
-# Copyright 2019 Creu Blanca
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# © 2017 Creu Blanca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 import base64
 import logging
+from io import BytesIO
 
-from odoo import exceptions
-from odoo.tests.common import TransactionCase
+import mock
+from odoo.addons.base.models.ir_actions_report import IrActionsReport
+from odoo.addons.component.tests.common import SavepointComponentRegistryCase
+from odoo.tests import common
 from odoo.tools.config import config
 
 _logger = logging.getLogger(__name__)
-
 try:
     from cryptography.fernet import Fernet
+    from PyPDF2 import PdfFileReader, PdfFileWriter
 except ImportError as err:
     _logger.debug(err)
 
 
-class TestInvoiceIntegration(TransactionCase):
-    def setUp(self):
-        super().setUp()
+class EDIBackendTestCase(SavepointComponentRegistryCase, common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+
+        self = cls
         config["email_integration_key"] = Fernet.generate_key()
-        self.state = self.env["res.country.state"].create(
-            {
-                "name": "Ciudad Real",
-                "code": "13",
-                "country_id": self.ref("base.es"),
-            }
-        )
-        self.partner = self.env["res.partner"].create(
-            {
-                "name": "NAME",
-                "street": "C/ Ejemplo, 13",
-                "zip": "13700",
-                "city": "Tomelloso",
-                "state_id": self.state.id,
-                "country_id": self.ref("base.es"),
-                "email": "noreply@test.com",
-                "vat": "ES05680675C",
-                "facturae": True,
-                "organo_gestor": "1",
-                "unidad_tramitadora": "1",
-                "email_integration_password": self.env[
-                    "account.invoice.integration.log"
-                ]._encrypt_value("TEST PASSWORD"),
-                "oficina_contable": "1",
-                "invoice_integration_method_ids": [
-                    (
-                        6,
-                        0,
-                        [
-                            self.env.ref(
-                                "invoice_integration_email.integration_email"
-                            ).id
-                        ],
-                    )
-                ],
-            }
-        )
-        main_company = self.env.ref("base.main_company")
-        main_company.vat = "ESA12345674"
-        main_company.partner_id.country_id = self.ref("base.uk")
-        main_company.currency_id = self.ref("base.EUR")
-        self.payment_method = self.env["account.payment.method"].create(
-            {
-                "name": "inbound_mandate",
-                "code": "inbound_mandate",
-                "payment_type": "inbound",
-                "bank_account_required": False,
-                "mandate_required": True,
-                "active": True,
-            }
-        )
-        payment_methods = self.env["account.payment.method"].search(
-            [("payment_type", "=", "inbound")]
-        )
-        self.journal = self.env["account.journal"].create(
-            {
-                "name": "Test journal",
-                "code": "TEST",
-                "type": "bank",
-                "company_id": main_company.id,
-                "inbound_payment_method_ids": [(6, 0, payment_methods.ids)],
-            }
-        )
-        self.payment_mode = self.env["account.payment.mode"].create(
-            {
-                "name": "Test payment mode",
-                "bank_account_link": "fixed",
-                "fixed_journal_id": self.journal.id,
-                "payment_method_id": self.env.ref(
-                    "payment.account_payment_method_electronic_in"
-                ).id,
-                "facturae_code": "01",
-            }
-        )
-
-        self.payment_mode_02 = self.env["account.payment.mode"].create(
-            {
-                "name": "Test payment mode 02",
-                "bank_account_link": "fixed",
-                "fixed_journal_id": self.journal.id,
-                "payment_method_id": self.payment_method.id,
-                "facturae_code": "02",
-            }
-        )
-        self.env["res.currency.rate"].search(
-            [("currency_id", "=", main_company.currency_id.id)]
-        ).write({"company_id": False})
-        bank_obj = self.env["res.partner.bank"]
-        self.bank = bank_obj.search(
-            [("acc_number", "=", "BE96 9988 7766 5544")], limit=1
-        )
-        if not self.bank:
-            self.bank = bank_obj.create(
-                {
-                    "state": "iban",
-                    "acc_number": "BE96 9988 7766 5544",
-                    "partner_id": self.partner.id,
-                    "bank_id": self.env["res.bank"]
-                    .search([("bic", "=", "PSSTFRPPXXX")], limit=1)
-                    .id,
-                }
-            )
-        self.mandate = self.env["account.banking.mandate"].create(
-            {
-                "company_id": main_company.id,
-                "format": "basic",
-                "partner_id": self.partner.id,
-                "state": "valid",
-                "partner_bank_id": self.bank.id,
-                "signature_date": "2016-03-12",
-            }
-        )
-
-        account = self.env["account.account"].create(
-            {
-                "company_id": main_company.id,
-                "name": "Facturae Product account",
-                "code": "facturae_product",
-                "user_type_id": self.env.ref(
-                    "account.data_account_type_revenue"
-                ).id,
-            }
-        )
-        self.invoice = self.env["account.invoice"].create(
-            {
-                "partner_id": self.partner.id,
-                "account_id": self.partner.property_account_receivable_id.id,
-                "journal_id": self.journal.id,
-                "date_invoice": "2016-03-12",
-                "partner_bank_id": self.bank.id,
-                "payment_mode_id": self.payment_mode.id,
-            }
-        )
-        main_company.facturae_cert_password = "password"
+        self._load_module_components(self, "component_event")
+        self._load_module_components(self, "edi")
+        self._load_module_components(self, "edi_account")
+        self._load_module_components(self, "edi_account_mail")
+        self._load_module_components(self, "edi_account_mail_encrypted")
         self.tax = self.env["account.tax"].create(
             {
                 "name": "Test tax",
@@ -163,76 +40,117 @@ class TestInvoiceIntegration(TransactionCase):
                 "facturae_code": "01",
             }
         )
-        self.invoice_line = self.env["account.invoice.line"].create(
+
+        self.partner = self.env["res.partner"].create(
             {
-                "product_id": self.env.ref("product.product_delivery_02").id,
-                "account_id": account.id,
-                "invoice_id": self.invoice.id,
-                "name": "Producto de prueba",
-                "quantity": 1.0,
-                "price_unit": 100.0,
-                "invoice_line_tax_ids": [(6, 0, self.tax.ids)],
+                "name": "Cliente de prueba",
+                "street": "C/ Ejemplo, 13",
+                "zip": "13700",
+                "city": "Tomelloso",
+                "country_id": self.env.ref("base.es").id,
+                "vat": "ES05680675C",
+                "send_invoice_by_mail": True,
+                "email_integration": "demo@demo.es",
+                "invoice_report_email_id": self.env.ref(
+                    "account.account_invoices"
+                ).id,
             }
         )
-        self.invoice._onchange_invoice_line_ids()
-        self.invoice.action_invoice_open()
-        self.invoice.number = "R/0001"
-
-    def test_process(self):
-        self.invoice.action_integrations()
-        integration = self.env["account.invoice.integration"].search(
-            [("invoice_id", "=", self.invoice.id)]
-        )
-        self.assertEqual(integration.method_id.code, "email")
-        self.assertEqual(integration.can_send, True)
-        attachment = self.env["ir.attachment"].create(
+        self.password = "1234"
+        self.env["res.encrypt.value"].with_context(
+            default_res_id=self.partner.id,
+            default_model=self.partner._name,
+            default_field="email_integration_password",
+        ).create({"value": self.password}).encrypt_store()
+        main_company = self.env.ref("base.main_company")
+        main_company.vat = "ESA12345674"
+        main_company.partner_id.country_id = self.env.ref("base.uk")
+        self.sale_journal = self.env["account.journal"].create(
             {
-                "name": "attach.txt",
-                "datas": base64.b64encode(b"attachment"),
-                "datas_fname": "attach.txt",
-                "res_model": "account.invoice",
-                "res_id": self.invoice.id,
-                "mimetype": "text/plain",
+                "name": "Sale journal",
+                "code": "SALE_TEST",
+                "type": "sale",
+                "company_id": main_company.id,
             }
         )
-        integration.attachment_ids = [(6, 0, attachment.ids)]
-        integration.send_action()
-        self.assertEqual(integration.state, "sent")
-        with self.assertRaises(exceptions.UserError):
-            integration.update_action()
-        with self.assertRaises(exceptions.ValidationError):
-            self.env["account.invoice.integration.cancel"].create(
-                {"integration_id": integration.id}
-            ).cancel_integration()
-        return integration
-
-    def test_process_email(self):
-        self.partner.email_integration = "test@test.com"
-        self.test_process()
-
-    def test_process_report(self):
-        self.partner.invoice_report_email_id = self.browse_ref(
-            "account.account_invoices_without_payment"
-        )
-        self.test_process()
-
-    def test_encryption(self):
-        passwrd = "TEST PASSWORD TEST"
-        self.env["res.encrypt.value"].create(
+        self.account = self.env["account.account"].create(
             {
-                "model": self.partner._name,
-                "res_id": self.partner.id,
-                "value": passwrd,
-                "field": "email_integration_password",
+                "company_id": main_company.id,
+                "name": "Facturae Product account",
+                "code": "facturae_product",
+                "user_type_id": self.env.ref(
+                    "account.data_account_type_revenue"
+                ).id,
             }
-        ).doit()
-        val = (
+        )
+        self.move = self.env["account.move"].create(
+            {
+                "partner_id": self.partner.id,
+                # "account_id": self.partner.property_account_receivable_id.id,
+                "journal_id": self.sale_journal.id,
+                "invoice_date": "2016-03-12",
+                "type": "out_invoice",
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.env.ref(
+                                "product.product_delivery_02"
+                            ).id,
+                            "account_id": self.account.id,
+                            "name": "Producto de prueba",
+                            "quantity": 1.0,
+                            "price_unit": 100.0,
+                            "tax_ids": [(6, 0, self.tax.ids)],
+                        },
+                    )
+                ],
+            }
+        )
+        self.move.refresh()
+
+    def test_check_password(self):
+        view_value = (
             self.env["res.view.value"]
             .with_context(
-                default_model=self.partner._name,
                 default_res_id=self.partner.id,
+                default_model=self.partner._name,
                 default_field="email_integration_password",
             )
             .create({})
         )
-        self.assertEqual(passwrd, val.value)
+        self.assertEqual(view_value.value, self.password)
+
+    def test_send(self):
+        self.assertTrue(self.partner.email_integration_password)
+        with mock.patch.object(IrActionsReport, "_run_wkhtmltopdf") as patch:
+            writer = PdfFileWriter()
+            writer.addBlankPage(219, 297)
+            buff = BytesIO()
+            writer.write(buff)
+            patch.return_value = buff.getvalue()
+            self.move.with_context(
+                force_edi_send=True,
+                _edi_send_break_on_error=True,
+                force_report_rendering=True,
+            ).post()
+            patch.assert_called()
+        self.assertTrue(self.move.exchange_record_ids)
+        self.assertRegex(
+            self.move.exchange_record_ids.exchange_filename, r"^.*\.pdf"
+        )
+        data = base64.b64decode(self.move.exchange_record_ids.exchange_file)
+        in_buff = BytesIO(data)
+        pdf = PdfFileReader(in_buff)
+        self.assertTrue(pdf.decrypt(self.password))
+
+    def test_send_zip(self):
+        self.assertTrue(self.partner.email_integration_password)
+        self.move.with_context(
+            force_edi_send=True, _edi_send_break_on_error=True,
+        ).post()
+        self.assertTrue(self.move.exchange_record_ids)
+        self.assertRegex(
+            self.move.exchange_record_ids.exchange_filename, r"^.*\.zip$"
+        )
--- edi_account_mail_encrypted/wizards/res_encrypt_value.py
+++ edi_account_mail_encrypted/wizards/res_encrypt_value.py
@@ -1,20 +1,20 @@
 # Copyright 2018 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ResEncryptValue(models.TransientModel):
     _name = "res.encrypt.value"
     _inherit = "email.encryptor"
+    _description = "Encrypt a mail value"
 
     model = fields.Char()
     res_id = fields.Integer(required=True)
     value = fields.Char(required=True)
     field = fields.Char(required=True)
 
-    @api.multi
-    def doit(self):
+    def encrypt_store(self):
         self.env[self.model].browse(self.res_id).write(
             {self.field: self._encrypt_value(self.value)}
         )
