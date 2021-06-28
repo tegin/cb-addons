ab8becde118e7d09e581230bf467a86cf07937a0
Olga Marco <olga.marco@creublanca.es>
2021-06-28 09:52:41 +0200

    [MIG] sale_commission_cancel: Migration to 13.0

--- sale_commission_cancel/__manifest__.py
+++ sale_commission_cancel/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Sale Commission Cancel",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Eficent, Creu Blanca",
     "website": "http://github.com/eficent/cb-addons",
     "summary": "Creates inter company relations",
