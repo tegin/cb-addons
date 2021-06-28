0e21a076af68a93a13cca5891b0d96d84d207d63
Enric Tobella <etobella@creublanca.es>
2021-06-28 10:10:53 +0200

    [MIG] web_icon_widget: Migration to 13.0

--- web_icon_widget/__manifest__.py
+++ web_icon_widget/__manifest__.py
@@ -2,8 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Flag Box",
-    "version": "12.0.1.0.0",
+    "name": "Icon Widget",
+    "version": "13.0.1.0.0",
     "author": "Creu Blanca",
     "license": "AGPL-3",
     "category": "Website",
