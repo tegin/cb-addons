d80adf55e90f5b68761e8b5c4fd04181d2f0503e
Enric Tobella <etobella@creublanca.es>
2021-06-29 18:13:22 +0200

    [MIG] sequence_safe: migration to 13.0

--- sequence_safe/models/ir_sequence.py
+++ sequence_safe/models/ir_sequence.py
@@ -16,18 +16,20 @@ class IrSequence(models.Model):
         "create the number. This way, we ensure the unicity of the code.",
     )
 
-    def _next(self):
+    def _next(self, sequence_date=None):
         if (
             getattr(threading.currentThread(), "testing", False)
             or self.env.context.get("install_mode")
             or self.env.context.get("ignore_safe", not self.safe)
         ):
-            return super()._next()
+            return super()._next(sequence_date=sequence_date)
         new_cr = Registry(self.env.cr.dbname).cursor()
         try:
             env = api.Environment(new_cr, self.env.uid, self.env.context)
             res = env[self._name].browse(self.id)
-            result = res.with_context(ignore_safe=True)._next()
+            result = res.with_context(ignore_safe=True)._next(
+                sequence_date=sequence_date
+            )
             new_cr.commit()
         except Exception:
             new_cr.rollback()  # error, rollback everything atomically
