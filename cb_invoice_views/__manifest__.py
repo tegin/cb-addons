27a769dd3957f577dd37ea74d66372ad5adb03fe
Olga Marco <olga.marco@creublanca.es>
2021-06-29 16:26:07 +0200

    [MIG] cb_invoice_views: Migration to 13.0

--- cb_invoice_views/__manifest__.py
+++ cb_invoice_views/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Cb Invoice Views",
     "summary": """
         Modify invoice views css""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
