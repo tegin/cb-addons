8791dc29726776803c1d8c0b2a8a9762f7f19cb7
Alba Riera <alba.riera@creublanca.es>
2021-06-30 13:01:58 +0200

    [MIG] cb_stock_location_warning: Migration to 13.0

--- cb_stock_location_warning/__manifest__.py
+++ cb_stock_location_warning/__manifest__.py
@@ -5,10 +5,10 @@
     "name": "Cb Stock Location Warning",
     "summary": """
         Add warnings to stock locations""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
-    "depends": ["purchase", "stock"],
+    "depends": ["stock_location_address_purchase"],
     "data": ["views/purchase_order_templates.xml", "views/stock_location.xml"],
 }
