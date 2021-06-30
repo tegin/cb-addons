fba686705208fdd71ede1600c039ad325d56be25
Alba Riera <alba.riera@creublanca.es>
2021-06-30 16:59:30 +0200

    [MIG] product_comercial: Migration to 13.0

--- product_comercial/__manifest__.py
+++ product_comercial/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Comercial",
     "summary": """
         Add Comercial field to product""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
