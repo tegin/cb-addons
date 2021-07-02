e4d6b829916ba04828bab3f54a07bd463fba7d43
Alba Riera <alba.riera@creublanca.es>
2021-07-02 10:46:23 +0200

    [MIG] stock_putaway_menu: Migration to 13.0

--- stock_putaway_menu/__manifest__.py
+++ stock_putaway_menu/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Putaway Menu",
     "summary": """
         Show menu for putaway strategies""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "www.creublanca.es",
