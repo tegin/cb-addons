201f40439168e79970162bfcaf7c34f0fd17e3ec
Enric Tobella <etobella@creublanca.es>
2021-07-05 11:47:39 +0200

    Revert "[FIX] pos_close_approval setup"

--- setup/pos_close_approval/setup.py
+++ setup/pos_close_approval/setup.py
@@ -2,9 +2,5 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon={
-        "depends_override": {
-            "pos_session_pay_invoice": "git+https://github.com/OCA/pos.git@refs/pull/674/merge#subdirectory=setup/pos_session_pay_invoice"
-        }
-    },
+    odoo_addon=True,
 )
