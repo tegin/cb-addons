a8a5e5a43e57885c7a1601179e9a93b9d64183e8
Olga Marco <olga.marco@creublanca.es>
2021-07-05 15:14:46 +0200

    [MIG] cb_mgmtsystem_issue_purchase: Migration to 13.0

--- cb_mgmtsystem_issue_purchase/__manifest__.py
+++ cb_mgmtsystem_issue_purchase/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Cb Management system Issue Purchase",
     "summary": """Cb Management system Issue Purchase""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
