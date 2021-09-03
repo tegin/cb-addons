25e75749f214d1cbd310f487c8a7786ed7bcd424
Enric Tobella <etobella@creublanca.es>
2021-09-03 11:58:58 +0200

    [IMP] add_close_approval: Add hook to allow edition

--- pos_close_approval/__init__.py
+++ pos_close_approval/__init__.py
@@ -4,3 +4,4 @@
 
 from . import models
 from . import wizard
+from .hooks import post_load_hook
---
+++ pos_close_approval/hooks.py
@@ -0,0 +1,29 @@
+from odoo import _
+from odoo.addons.point_of_sale.models.pos_config import PosConfig
+from odoo.exceptions import UserError
+
+
+def post_load_hook():  # noqa: C901
+    if not hasattr(PosConfig, "write_close_approval_original"):
+        old_function = PosConfig.write
+        PosConfig.write_close_approval_original = old_function
+
+    def new_write(self, vals):
+        opened_session = self.mapped("session_ids").filtered(
+            lambda s: s.state not in ["closed", "pending_approval"]
+        )
+        if opened_session:
+            raise UserError(
+                _(
+                    "Unable to modify this PoS Configuration because there "
+                    "is an open PoS Session based on it."
+                )
+            )
+        result = super(PosConfig, self).write(vals)
+
+        self.sudo()._set_fiscal_position()
+        self.sudo()._check_modules_to_install()
+        self.sudo()._check_groups_implied()
+        return result
+
+    PosConfig.write = new_write
