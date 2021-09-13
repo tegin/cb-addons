474856fe80dd11331848b477fa35731c4a3adfd6
Alba Riera <alba.riera@creublanca.es>
2021-09-13 17:18:46 +0200

    [MIG] queue_nsca_client: Migration to 13.0

--- queue_nsca_client/__manifest__.py
+++ queue_nsca_client/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Queue - NSCA Client",
     "summary": "Send passive alerts to monitor your Odoo application.",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Tools",
     "website": "http://github.com/OCA/server-tools",
     "author": "Creu Blanca, Odoo Community Association (OCA)",
