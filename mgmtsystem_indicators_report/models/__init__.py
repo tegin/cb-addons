0d0e2b84b0bb7b9260f07a999c10b32eaf9aea3d
Alba Riera <alba.riera@creublanca.es>
2021-11-12 10:20:30 +0100

    [ADD] mgmtsystem_indicators_report

---
+++ mgmtsystem_indicators_report/models/__init__.py
@@ -0,0 +1,6 @@
+from . import mgmtsystem_indicator_report_abstract
+from . import mgmtsystem_indicator
+from . import mgmtsystem_indicators_report
+from . import mgmtsystem_indicator_concept
+from . import mgmtsystem_indicators_report_template
+from . import uom_uom
