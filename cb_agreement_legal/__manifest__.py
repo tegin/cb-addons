f58e1b42cc04c39c534808b01eb9d2cc94519224
Kevin Luna <kevin.luna@creublanca.es>
2022-11-11 10:11:35 +0100

    [MIG] cb_agreement_legal: Migration to 14.0

--- cb_agreement_legal/__manifest__.py
+++ cb_agreement_legal/__manifest__.py
@@ -4,12 +4,12 @@
 {
     "name": "Agreements Legal Demo",
     "summary": "Create contract from agreement",
-    "author": "Creu Blanca, Odoo Community Association (OCA)",
+    "author": "CreuBlanca, Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-addons",
     "category": "Agreement",
     "depends": ["agreement_legal", "archive_management"],
     "license": "AGPL-3",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "data": [
         "data/agreement_demo_data.xml",
         "views/agreement.xml",
--- setup/cb_agreement_legal/setup.py
+++ setup/cb_agreement_legal/setup.py
@@ -2,5 +2,9 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        "depends_override": {
+            "archive_management": "git+https://github.com/tegin/archive-management.git@14.0#subdirectory=setup/archive_management"
+        }
+    },
 )
