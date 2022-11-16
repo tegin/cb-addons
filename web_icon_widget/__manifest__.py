7eb2dd657ddc2129ed4d1a40ba611e78c813118c
Kevin Luna <kevin.luna@creublanca.es>
2022-11-16 12:19:18 +0100

    [MIG] web_icon_widget: migration to 14.0

--- web_icon_widget/__manifest__.py
+++ web_icon_widget/__manifest__.py
@@ -3,8 +3,9 @@
 
 {
     "name": "Icon Widget",
-    "version": "13.0.1.0.0",
-    "author": "Creu Blanca",
+    "version": "14.0.1.0.0",
+    "author": "CreuBlanca",
+    "website": "https://github.com/tegin/cb-addons",
     "license": "AGPL-3",
     "category": "Website",
     "summary": "Allows to use a flag box with icons / buttons",
