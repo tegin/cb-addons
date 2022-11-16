4976a84d1c03be7d442df68af3f7a4a219d92642
Kevin Luna <kevin.luna@creublanca.es>
2022-11-16 10:22:03 +0100

    [MIG] cb_sale_report_invoice: migration to 14.0

--- cb_sale_report_invoice/__manifest__.py
+++ cb_sale_report_invoice/__manifest__.py
@@ -5,9 +5,9 @@
     "name": "Cb Sale Report Invoice",
     "summary": """
         Sale Report invoice modification for CB""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca",
+    "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-addons",
     "depends": ["sale"],
     "data": ["views/report_invoice.xml"],
