c99663323a603b96dbc2161888f1c171bc5e1b39
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-16 12:40:45 +0100

    [MIG] mgmtsystem_indicators_report: Migration to 14.0

--- mgmtsystem_indicators_report/static/src/js/indicator_value_widget.js
+++ mgmtsystem_indicators_report/static/src/js/indicator_value_widget.js
@@ -4,7 +4,6 @@ odoo.define("mgmtsystem_indicators_report.ValueWidget", function (require) {
        This code modifies the "group" tag allowing to add
        a group of fields as a single field in a table column.
     */
-    var field_utils = require("web.field_utils");
     var ListRenderer = require("web.ListRenderer");
     var relational_fields = require("web.relational_fields");
     ListRenderer.include({
@@ -20,75 +19,30 @@ odoo.define("mgmtsystem_indicators_report.ValueWidget", function (require) {
             }
             this._super.apply(this, arguments);
         },
-        _renderHeaderCell: function (node) {
-            var $th = this._super.apply(this, arguments);
-            var name = node.attrs.name;
-            if (node.tag === "group" && !this.state.fields[name] && node.attrs.string) {
-                $th.text(node.attrs.string);
-            }
-            return $th;
-        },
-
         _renderBodyCell: function (record, node, colIndex, options) {
-            if (node.tag === "group") {
-                var $td = $("<td>", {class: "o_data_cell o_field_cell"});
-                var modifiers = this._registerModifiers(
-                    node,
-                    record,
-                    $td,
-                    _.pick(options, "mode")
-                );
-                // If the invisible modifiers is true, the <td> element is left empty.
-                // Indeed, if the modifiers was to change the whole cell would be
-                // rerendered anyway.
-                if (modifiers.invisible && !(options && options.renderInvisible)) {
-                    return $td;
-                }
-                var self = this;
-                _.map(node.children, function (child_node) {
-                    var child_modifiers = self._registerModifiers(
-                        child_node,
-                        record,
-                        $td,
-                        _.pick(options, "mode")
-                    );
-                    // If the invisible modifiers is true, the <td> element is left empty.
-                    // Indeed, if the modifiers was to change the whole cell would be
-                    // rerendered anyway.
-                    if (child_modifiers.invisible) {
-                        return;
+            if (
+                node.tag === "field" &&
+                node.attrs &&
+                node.attrs.widget === "mgmtsystem_indicator_report" &&
+                record.data.value_type
+            ) {
+                var final_child = undefined;
+                _.each(node.children, function (child) {
+                    if (child.attrs.name === "value_" + record.data.value_type) {
+                        final_child = child;
                     }
-                    if (child_node.tag === "button") {
-                        return $td.append(self._renderButton(record, child_node));
-                    } else if (child_node.tag === "widget") {
-                        return $td.append(self._renderWidget(record, child_node));
-                    }
-                    if (child_node.attrs.widget || (options && options.renderWidgets)) {
-                        var $el = self._renderFieldWidget(
-                            child_node,
-                            record,
-                            _.pick(options, "mode")
-                        );
-                        self._handleAttributes($el, child_node);
-                        return $td.append($el);
-                    }
-                    var name = child_node.attrs.name;
-                    var field = self.state.fields[name];
-                    var value = record.data[name];
-                    var formattedValue = field_utils.format[field.type](value, field, {
-                        data: record.data,
-                        escape: true,
-                        isPassword: "password" in child_node.attrs,
-                        digits: child_node.attrs.digits
-                            ? JSON.parse(child_node.attrs.digits)
-                            : undefined,
-                    });
-                    self._handleAttributes($td, child_node);
-                    return $td.html(formattedValue);
                 });
-                return $td;
+                console.log(final_child);
+                if (final_child !== undefined) {
+                    if (typeof final_child.attrs.modifiers !== "object") {
+                        final_child.attrs.modifiers = final_child.attrs.modifiers
+                            ? JSON.parse(final_child.attrs.modifiers)
+                            : {};
+                    }
+                    console.log(final_child, record, colIndex, options);
+                    return this._renderBodyCell(record, final_child, colIndex, options);
+                }
             }
-
             return this._super.apply(this, arguments);
         },
     });
