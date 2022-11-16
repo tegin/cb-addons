ef773868c63a89ffa6fa61a81d518adc562fe1b7
Kevin Luna <kevin.luna@creublanca.es>
2022-11-16 10:34:41 +0100

    [IMP] cb_stock_location_warning: black, isort, prettier

--- cb_stock_location_warning/__manifest__.py
+++ cb_stock_location_warning/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
-    "website": "www.creublanca.es",
+    "website": "https://github.com/tegin/cb-addons",
     "depends": ["stock_location_address_purchase"],
     "data": ["views/purchase_order_templates.xml", "views/stock_location.xml"],
 }
