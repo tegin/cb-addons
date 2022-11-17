5e03bc499402d8dcd1ef9938881677f13406fa1a
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-17 10:31:47 +0100

    [14.0][MIG] document_quick_access_label: Migration to 14.0

--- document_quick_access_label/__manifest__.py
+++ document_quick_access_label/__manifest__.py
@@ -5,9 +5,9 @@
     "name": "Document Quick Access Label",
     "summary": """
         Allows to print labels from Document Quick Access records""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-addons",
     "depends": [
         "document_quick_access",
