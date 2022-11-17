99dda7157e20042b4b647abec0c5b5ee0124f43f
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-17 10:13:03 +0100

    [IMP] document_quick_access_label: black, isort, prettier

--- document_quick_access_label/__manifest__.py
+++ document_quick_access_label/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/server-ux",
+    "website": "https://github.com/tegin/cb-addons",
     "depends": [
         "document_quick_access",
         "printer_zpl2",
