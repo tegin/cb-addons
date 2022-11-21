4f4a3ab4fe0eccc2a3f906286d850330930030c7
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-21 17:16:36 +0100

    [IMP] account_journal_inter_company: Adapt tests

--- account_journal_inter_company/tests/test_intercompany.py
+++ account_journal_inter_company/tests/test_intercompany.py
@@ -3,10 +3,12 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo.exceptions import ValidationError
+from odoo.tests.common import tagged
 
 from . import common
 
 
+@tagged("post_install", "-at_install")
 class TestInterCompanyJournal(common.TestInterCompany):
     def test_intercompany(self):
         self.create_inter_company(self.company_1, self.company_2)
@@ -16,6 +18,7 @@ class TestInterCompanyJournal(common.TestInterCompany):
             self.company_1.inter_company_ids.inter_company_id,
             self.company_2.inter_company_ids,
         )
+        self.company_1.refresh()
         self.assertTrue(
             self.company_1.related_company_ids.filtered(
                 lambda r: r.id == self.company_2.id
