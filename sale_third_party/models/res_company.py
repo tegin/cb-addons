0bf9d71c45ff94a7503e28b878d679d126f98426
Enric Tobella <etobella@creublanca.es>
2022-11-22 12:27:53 +0100

    [FIX] sale_third_party: Finish migration

--- sale_third_party/models/res_company.py
+++ sale_third_party/models/res_company.py
@@ -26,6 +26,58 @@ class ResCompany(models.Model):
         "account.journal", domain="[('company_id', '=', active_id)]"
     )
 
+    def get_property_value(self, model, field):
+        value = self.env["ir.property"].with_company(self).sudo()._get(field, model)
+        if value:
+            if isinstance(value, list):
+                return value[0]
+            else:
+                return value
+        return False
+
+    def set_property_value(self, model, field_name, value):
+        field = (
+            self.env["ir.model.fields"]
+            .sudo()
+            .search([("name", "=", field_name), ("model", "=", model)], limit=1)
+        )
+        if isinstance(value, models.BaseModel):
+            if value:
+                val = value.id
+            else:
+                val = False
+        else:
+            val = value
+        prop = (
+            self.env["ir.property"]
+            .sudo()
+            .search(
+                [
+                    ("name", "=", field_name),
+                    ("fields_id", "=", field.id),
+                    ("company_id", "=", self.id),
+                    ("res_id", "=", False),
+                ]
+            )
+        )
+        if not prop:
+            prop = (
+                self.env["ir.property"]
+                .sudo()
+                .create(
+                    {
+                        "name": field_name,
+                        "fields_id": field.id,
+                        "company_id": self.id,
+                        "res_id": False,
+                    }
+                )
+            )
+        if isinstance(value, models.BaseModel) and not val:
+            prop.sudo().unlink()
+        else:
+            prop.sudo().write({"value": val})
+
     @api.model
     def _compute_third_party_customer_account(self):
         for rec in self:
