996417562d2e368d01ee4ead47ccdb962e57b102
Núria Sancho <nuria.sancho@creublanca.es>
2022-11-22 11:13:05 +0100

    [14.0][MIG] edi_account_mail: Migration to 14.0

--- edi_account_mail/__manifest__.py
+++ edi_account_mail/__manifest__.py
@@ -5,10 +5,10 @@
     "name": "Invoice Integration Email",
     "summary": """
         Send invoices through emails as an integration method""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca",
+    "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-addons",
-    "depends": ["edi_account"],
+    "depends": ["edi_account_oca"],
     "data": ["data/method_data.xml", "views/res_partner_view.xml"],
 }
