f5a815a3469bd7708fde123aa12eb806b280bb81
Enric Tobella <etobella@creublanca.es>
2023-01-28 23:44:38 +0100

    [FIX] cb_mgmtsystem_issue_product: Fix view

--- cb_mgmtsystem_issue_product/models/product_template.py
+++ cb_mgmtsystem_issue_product/models/product_template.py
@@ -8,3 +8,10 @@ class ProductTemplate(models.Model):
 
     _name = "product.template"
     _inherit = ["product.template", "mgmtsystem.quality.issue.abstract"]
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    def action_view_quality_issues(self):
+        return self.product_tmpl_id.action_view_quality_issues()
