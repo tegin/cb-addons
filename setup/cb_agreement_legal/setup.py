6cefc9cf593b802898fc7f37b3788d14e8dcd1b8
Enric Tobella <enric.tobella@dixmit.com>
2024-02-13 18:11:00 +0100

    fix pre-commit

--- setup/cb_agreement_legal/setup.py
+++ setup/cb_agreement_legal/setup.py
@@ -2,9 +2,5 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon={
-        "depends_override": {
-            "archive_management": "git+https://github.com/tegin/archive-management.git@14.0#subdirectory=setup/archive_management"
-        }
-    },
+    odoo_addon={},
 )
