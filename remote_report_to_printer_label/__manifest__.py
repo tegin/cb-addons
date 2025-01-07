01facda43c4ca8bdb0b345eb4c1ac27aa239a516
Enric Tobella <enric.tobella@dixmit.com>
2025-01-07 11:07:10 +0100

    [MIG] remote_report_to_printer_label: Migration to 16.0

--- remote_report_to_printer_label/__manifest__.py
+++ remote_report_to_printer_label/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Remote Report To Printer Label",
     "summary": """
         Adds label selection to remote printer""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-addons",
