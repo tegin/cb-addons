914c96b6437bbc07e7983050b5728771d774c5d6
Enric Tobella <enric.tobella@dixmit.com>
2025-01-07 12:40:05 +0100

    [MIG] document_quick_access_report_qr: migration to 16.0

--- document_quick_access_report_qr/__manifest__.py
+++ document_quick_access_report_qr/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Document Quick Access Report Qr",
     "summary": """
         Add QR to models reports""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-addons",
