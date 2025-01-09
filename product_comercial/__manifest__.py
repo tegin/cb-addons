a95759573d8347b0c7ab685652a501d7feae8ebd
Enric Tobella <enric.tobella@dixmit.com>
2025-01-09 13:00:03 +0100

    [MIG] product_comercial: Migration to 16.0

--- product_comercial/__manifest__.py
+++ product_comercial/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Comercial",
     "summary": """
         Add Comercial field to product""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-addons",
