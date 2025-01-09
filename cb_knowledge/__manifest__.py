6890ccb2cf3eb0906b9d5576a136f3b007c8791e
Kevin Luna <kevin.luna@creublanca.es>
2025-01-09 12:22:23 +0100

    [MIG] cb_knowledge: Migration to 16.0

--- cb_knowledge/__manifest__.py
+++ cb_knowledge/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Cb Knowledge",
     "summary": """
         Knowledge for CB""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-addons",
