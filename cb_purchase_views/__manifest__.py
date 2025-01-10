269d3673d3d3edfb98cd6e829b2274bb8540b065
Kevin Luna <kevin.luna@creublanca.es>
2025-01-10 10:23:44 +0100

    [MIG] cb_purchase_views: Migration to 16.0

--- cb_purchase_views/__manifest__.py
+++ cb_purchase_views/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Cb Purchase Views",
     "summary": """
         Purchase custom views""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-addons",
