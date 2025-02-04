c0dac1977dd1c4373cf0ed16743a8c9610bcae52
Enric Tobella <enric.tobella@dixmit.com>
2025-02-04 15:00:11 +0100

    [IMP] edi_account_storage: Make it company dependant

--- edi_account_storage/models/res_partner.py
+++ edi_account_storage/models/res_partner.py
@@ -7,5 +7,7 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    account_invoice_storage_exchange_type_id = fields.Many2one("edi.exchange.type")
+    account_invoice_storage_exchange_type_id = fields.Many2one(
+        "edi.exchange.type", company_related=True
+    )
     account_invoice_storage_clean_file_name = fields.Boolean()
