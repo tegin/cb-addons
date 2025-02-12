4887decab3d2b32f47ba87ca8c03f308f8569065
Luis Rodriguez <luis.rodriguez@dixmit.com>
2025-02-12 11:41:03 +0100

    [FIX] edi_account_storage: Wrong field attribute name

--- edi_account_storage/models/res_partner.py
+++ edi_account_storage/models/res_partner.py
@@ -8,6 +8,6 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     account_invoice_storage_exchange_type_id = fields.Many2one(
-        "edi.exchange.type", company_related=True
+        "edi.exchange.type", company_dependent=True
     )
     account_invoice_storage_clean_file_name = fields.Boolean()
