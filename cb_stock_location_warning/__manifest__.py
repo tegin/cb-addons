4be99511b5ba1f51634b002aba64a8889925d41f
Kevin Luna <kevin.luna@creublanca.es>
2025-02-18 15:37:25 +0100

    [MIG] cb_stock_location_warning: migration to 16.0

--- cb_stock_location_warning/__manifest__.py
+++ cb_stock_location_warning/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Cb Stock Location Warning",
     "summary": """
         Add warnings to stock locations""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-addons",
