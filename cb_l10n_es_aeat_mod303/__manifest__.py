68b4d35bc1993e8410cb7bdfbff29c55e1322132
Kevin Luna <kevin.luna@creublanca.es>
2025-02-18 12:36:29 +0100

    [MIG] l10n_es_aeat_mod303: migration to 16.0

--- cb_l10n_es_aeat_mod303/__manifest__.py
+++ cb_l10n_es_aeat_mod303/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Cb L10n Es Aeat Mod303",
     "summary": """
         Add report to mod 303""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-addons",
