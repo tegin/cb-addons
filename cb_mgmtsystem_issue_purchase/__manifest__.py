083febc1243cf00dd2477530899dca5a1ccd9f12
Kevin Luna <kevin.luna@creublanca.es>
2025-02-18 14:00:59 +0100

    [MIG] cb_mgmtsystem_issue_purchase: migration to 16.0

--- cb_mgmtsystem_issue_purchase/__manifest__.py
+++ cb_mgmtsystem_issue_purchase/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Cb Management system Issue Purchase",
     "summary": """Cb Management system Issue Purchase""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-addons",
