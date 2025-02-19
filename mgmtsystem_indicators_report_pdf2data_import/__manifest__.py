1b9a0f444274ee4693d19359cd914d9b1ae42598
Kevin Luna <kevin.luna@creublanca.es>
2025-02-19 09:50:49 +0100

    [MIG] mgmsystem_indicators_report_pdf2data_import: migration to 16.0

--- mgmtsystem_indicators_report_pdf2data_import/__manifest__.py
+++ mgmtsystem_indicators_report_pdf2data_import/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         This addon allows to create a indicators report
         extracting the data from a pdf""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-addons",
