79da43aafa00fb11e76602318cc54aa1bcb949f9
Kevin Luna <kevin.luna@creublanca.es>
2025-02-20 11:14:53 +0100

    [MIG] sequence_safe: migration to 16.0

--- sequence_safe/__manifest__.py
+++ sequence_safe/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Check Digit on Sequences",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Reporting",
     "website": "https://github.com/tegin/cb-addons",
     "author": "CreuBlanca",
