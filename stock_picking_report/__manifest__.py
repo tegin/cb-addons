58e7fdb35d8dd8ab6f64e6dd1446af1e13e27155
Kevin Luna <kevin.luna@creublanca.es>
2025-02-20 12:19:30 +0100

    [MIG] stock_pricking_report: migration to 16.0

--- stock_picking_report/__manifest__.py
+++ stock_picking_report/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Picking Report",
     "summary": """
         Improve Picking report""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/cb-addons",
