311cf4a517058f9efb565e18c42ed8d2f9b7c9f9
Kevin Luna <kevin.luna@creublanca.es>
2025-03-05 16:55:59 +0100

    [MIG] web_single_app_logo: migration to 16.0

--- web_single_app_logo/static/src/js/web_client.esm.js
+++ web_single_app_logo/static/src/js/web_client.esm.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import {patch} from "@web/core/utils/patch";
 import {WebClient} from "@web/webclient/webclient";
+import {patch} from "@web/core/utils/patch";
 
 patch(WebClient.prototype, "web_single_app_logo.WebClient", {});
