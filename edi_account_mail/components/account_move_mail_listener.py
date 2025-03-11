e04a5522433a4779fde99a18506a0daeb216f415
Enric Tobella <enric.tobella@dixmit.com>
2025-03-11 12:09:16 +0100

    [FIX] edi_account_mail: Fix functions

--- edi_account_mail/components/account_move_mail_listener.py
+++ edi_account_mail/components/account_move_mail_listener.py
@@ -31,10 +31,8 @@ class AccountMoveMailListener(Component):
             backend = self._get_backend(record)
             if not backend:
                 continue
-            exchange_type = (
-                partner.invoice_mail_exchange_type_id.code or "account_move_mail"
-            )
-            if record._has_exchange_record(exchange_type, backend):
+            exchange_type = partner.invoice_mail_exchange_type_id
+            if not exchange_type or record._has_exchange_record(exchange_type, backend):
                 continue
             exchange_record = backend.create_record(
                 exchange_type, self._get_exchange_record_vals(record)
