2a1c35396a8597f75968993a276d95c9fd63d9bb
Enric Tobella <enric.tobella@dixmit.com>
2025-03-29 09:36:08 +0100

    [FIX] sequence_safe: Remove flush

--- sequence_safe/models/ir_sequence.py
+++ sequence_safe/models/ir_sequence.py
@@ -30,7 +30,7 @@ class IrSequence(models.Model):
             result = res.with_context(ignore_safe=True)._next(
                 sequence_date=sequence_date
             )
-            res.flush()
+            res.flush_recordset()
             new_cr.commit()
         except Exception:
             new_cr.rollback()  # error, rollback everything atomically
